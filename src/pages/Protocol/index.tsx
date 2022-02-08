a5a7c03e6369c5c075f415b527803cd04042febd
Nathiely <nathielymm@gmail.com>
2022-02-08 09:54:50 -0300

    feat: add donationToken and donation balance button

--- src/pages/Protocol/index.tsx
+++ src/pages/Protocol/index.tsx
@@ -1,4 +1,5 @@
 import RibonAbi from "utils/abis/Ribon.json";
+import TokenAbi from "utils/abis/DonationToken.json";
 import { useWalletContext } from "contexts/walletContext";
 import { useContract } from "hooks/useContract";
 import { useNetwork } from "hooks/useNetwork";
@@ -8,8 +9,12 @@ function Protocol(): JSX.Element {
   const { wallet, connectWallet } = useWalletContext();
   const { isValidNetwork, currentNetwork } = useNetwork();
   const contract = useContract({
-    address: "0xF471e129127D04d1167f7dFb82c6C0ce3c92d387",
+    address: "0xA63B4b2cb11B79a535c7B8b9269E729b4d7cE056",
     ABI: RibonAbi.abi,
+  });
+  const donationToken = useContract({
+    address: "0x64F7AA155240c3D754C35CF56cb3f3AcaB488635",
+    ABI: TokenAbi.abi,
   });
 
   // async function getIntegrations() {
@@ -24,6 +29,11 @@ function Protocol(): JSX.Element {
     );
   }
 
+  async function addDonationPoolBalance() {
+    await donationToken?.approve(contract?.address, 2);
+    await contract?.addDonationPoolBalance(2);
+  }
+
   return (
     <S.Container>
       <h1>Protocol</h1>
@@ -32,11 +42,15 @@ function Protocol(): JSX.Element {
       </button>
       <p>wallet: {wallet}</p>
       <p>
-        network: {currentNetwork?.chainId} (
-        {isValidNetwork() ? "valid" : "invalid"})
+        {" "}
+        network: {currentNetwork?.chainId} ({" "}
+        {isValidNetwork() ? "valid" : "invalid"}){" "}
       </p>
       <button type="button" onClick={addNonProfitToWhitelist}>
-        teste
+        add whitelist
+      </button>
+      <button type="button" onClick={addDonationPoolBalance}>
+        add donation balance
       </button>
     </S.Container>
   );

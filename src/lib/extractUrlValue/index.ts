8b975c7fe575b0eb31396b992e9ee7ae0f4ea6a0
Nicholas Marques <nnmarques97@gmail.com>
2022-09-30 08:28:33 -0300

    feat: add donate voucher

---
+++ src/lib/extractUrlValue/index.ts
@@ -0,0 +1,10 @@
+export default function extractUrlValue(
+  key: string,
+  url: string,
+): string | null {
+  // the url is something like this ?redirect_to=/something
+  // this match will try to split into ?redirect_to= and /something
+  const match = url.match(`[?&]${key}=([^&]+)`);
+  const value = match && match[1];
+  return value || null;
+}

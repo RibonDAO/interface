451335219c438a83e0ac391c247ebccf60039f8c
Nicholas Marques <nnmarques97@gmail.com>
2022-09-30 09:14:52 -0300

    chore: update extractUrlValue tests

--- src/lib/extractUrlValue/index.test.ts
+++ src/lib/extractUrlValue/index.test.ts
@@ -15,7 +15,7 @@ describe("extractUrlValue", () => {
     expect(extractUrlValue(key, url)).toEqual(expectedPath);
   });
 
-  it("expects to return null", () => {
+  it("expects to return undefined", () => {
     const url = "https://localhost:300/auth";
     const key = "redirect_to";
     expect(extractUrlValue(key, url)).toBeUndefined();
--- src/lib/extractUrlValue/index.ts
+++ src/lib/extractUrlValue/index.ts
@@ -2,8 +2,6 @@ export default function extractUrlValue(
   key: string,
   url: string,
 ): string | undefined {
-  // the url is something like this ?redirect_to=/something
-  // this match will try to split into ?redirect_to= and /something
   const match = url.match(`[?&]${key}=([^&]+)`);
   const value = match && match[1];
   return value || undefined;

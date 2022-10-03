cfb30533bd506913d199d463f5eadef1968c6af8
let <let@MacBook-Air-de-let.local>
2022-10-03 17:04:50 -0300

    fix: remove localhost from constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,8 @@ export const RIBON_GROWTHBOOK_URL =
   "https://growthbook.ribon.io:444/api/features/key_prod_2161769c509d739b";
 
 export const RIBON_API =
-  process.env.REACT_APP_RIBON_API || "http://localhost:3000";
+  process.env.REACT_APP_RIBON_API ||
+  "http://ribon-core-api-dev.us-east-1.elasticbeanstalk.com/";
 
 export const THE_GRAPH_API =
   "https://api.thegraph.com/subgraphs/name/ribondao/subgraphribon";

a60e12a9bdcccbcc706a41fdc2bfc0e7b5fc5b21
Henrique Barbosa <henrikleod@gmail.com>
2022-10-19 11:54:51 -0300

    fix: dynamic height in chromium based browsers

--- src/pages/donations/StoriesPage/styles.ts
+++ src/pages/donations/StoriesPage/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
+  max-height: stretch;
   display: flex;
   align-items: center;
   justify-content: center;

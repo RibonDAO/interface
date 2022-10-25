a45e4c4632603cca3ed06de7a9069304950230e8
Nathiely Macedo <nathielymm@gmail.com>
2022-10-25 16:47:36 -0300

    fix: quotes string

--- src/services/api/causesApi/index.ts
+++ src/services/api/causesApi/index.ts
@@ -3,7 +3,7 @@ import Cause from "types/entities/Cause";
 import { apiGet } from "..";
 
 const causesApi = {
-  getCauses: (): Promise<AxiosResponse<Cause[]>> => apiGet(`causes/`),
+  getCauses: (): Promise<AxiosResponse<Cause[]>> => apiGet("causes/"),
 };
 
 export default causesApi;

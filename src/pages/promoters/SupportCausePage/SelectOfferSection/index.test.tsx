7a1b570a64168d43c0d4264b6814e4377445c2fa
Nicholas Marques <nnmarques97@gmail.com>
2022-10-31 14:58:47 -0300

    fix: update SelectOfferSection tests

--- src/pages/promoters/SupportCausePage/SelectOfferSection/index.test.tsx
+++ src/pages/promoters/SupportCausePage/SelectOfferSection/index.test.tsx
@@ -1,7 +1,18 @@
 import { renderComponent } from "config/testUtils";
 import { expectTextToBeInTheDocument } from "config/testUtils/expects";
 import causeFactory from "config/testUtils/factories/causeFactory";
+import offerFactory from "config/testUtils/factories/offerFactory";
 import OfferSelectionSection from ".";
+import { formatPrice } from "../../../../lib/formatters/currencyFormatter";
+
+const mockOffer = offerFactory();
+jest.mock("hooks/apiHooks/useOffers", () => ({
+  __esModule: true,
+  default: () => ({
+    offers: [mockOffer],
+    refetch: jest.fn(),
+  }),
+}));
 
 describe("OfferSelectionSection", () => {
   const mockFn = jest.fn();
@@ -12,9 +23,9 @@ describe("OfferSelectionSection", () => {
     );
   });
 
-  it("Shows available values", () => {
-    expectTextToBeInTheDocument("$5,00");
-    expectTextToBeInTheDocument("$10,00");
-    expectTextToBeInTheDocument("$15,00");
+  it("show the first offer", () => {
+    expectTextToBeInTheDocument(
+      formatPrice(mockOffer.priceValue, mockOffer.currency),
+    );
   });
 });

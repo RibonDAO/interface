9aa4d636d5c7203bd29cb7dd9bf5a1b2b466973d
Nicholas Marques <nnmarques97@gmail.com>
2022-10-31 16:08:40 -0300

    refactor: add absolute import

--- src/pages/promoters/SupportCausePage/SelectOfferSection/index.test.tsx
+++ src/pages/promoters/SupportCausePage/SelectOfferSection/index.test.tsx
@@ -2,8 +2,8 @@ import { renderComponent } from "config/testUtils";
 import { expectTextToBeInTheDocument } from "config/testUtils/expects";
 import causeFactory from "config/testUtils/factories/causeFactory";
 import offerFactory from "config/testUtils/factories/offerFactory";
+import { formatPrice } from "lib/formatters/currencyFormatter";
 import OfferSelectionSection from ".";
-import { formatPrice } from "../../../../lib/formatters/currencyFormatter";
 
 const mockOffer = offerFactory();
 jest.mock("hooks/apiHooks/useOffers", () => ({

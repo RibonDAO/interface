ded1b51e0ff5941f42e95e7f171f0cacd1671623
Nathiely Macedo <nathielymm@gmail.com>
2022-10-31 17:17:24 -0300

    fix: run prettier

--- src/pages/promoters/SupportCausePage/styles.ts
+++ src/pages/promoters/SupportCausePage/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import { stylizedHeadingLarge } from "styles/typography/stylized";
 
 export const Container = styled.div`
-  width: 100%;
 `;
 
 export const BackgroundImage = styled.img`

afdd8e8656a04cf404a4234fbcf51ba886892ee0
Nicholas Marques <nnmarques97@gmail.com>
2022-11-08 11:22:04 -0300

    feat: update initial state to card on SupportCause

--- src/pages/promoters/SupportCausePage/index.tsx
+++ src/pages/promoters/SupportCausePage/index.tsx
@@ -3,7 +3,7 @@ import CardPage from "./CardPage";
 import CryptoPage from "./CryptoPage";
 
 function SupportCausePage(): JSX.Element {
-  const [currentSection] = useState<"card" | "crypto">("crypto");
+  const [currentSection] = useState<"card" | "crypto">("card");
 
   return currentSection === "card" ? <CardPage /> : <CryptoPage />;
 }

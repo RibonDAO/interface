e1f58e16beddb9f16d4f2c36f33a4196b96ea1e2
Henrique Barbosa <henrikleod@gmail.com>
2022-11-09 06:20:25 -0300

    feat: allow static icons flag

--- src/components/atomics/sections/IconsAroundImage/index.stories.tsx
+++ src/components/atomics/sections/IconsAroundImage/index.stories.tsx
@@ -6,12 +6,21 @@ export default {
   component: IconsAroundImage,
 } as ComponentMeta<typeof IconsAroundImage>;
 
-const Template: ComponentStory<typeof IconsAroundImage> = function (args: Props) {
+const Template: ComponentStory<typeof IconsAroundImage> = function (
+  args: Props,
+) {
   return <IconsAroundImage {...args} />;
 };
 
 export const Default = Template.bind({});
 
+export const StaticIcons = Template.bind({});
+
 Default.args = {
   imageSrc: "https://picsum.photos/200",
+};
+
+StaticIcons.args = {
+  imageSrc: "https://picsum.photos/200",
+  staticIcons: true,
 };
--- src/components/atomics/sections/IconsAroundImage/index.tsx
+++ src/components/atomics/sections/IconsAroundImage/index.tsx
@@ -10,20 +10,29 @@ import * as S from "./styles";
 
 export type Props = {
   imageSrc: string;
-}
+  staticIcons?: boolean;
+};
 
-function IconsAroundImage({ imageSrc }: Props) {
+function IconsAroundImage({ imageSrc, staticIcons = false }: Props) {
   return (
     <S.Container>
-      <S.Icon src={VolunteerActivismPink} animationName="outer_orbit_1" />
-      <S.Icon src={FlareGreen} animationName="outer_orbit_2" />
-      <S.Icon src={VolunteerActivismYellow} animationName="outer_orbit_3" />
-      <S.Icon src={SparklesPink} animationName="outer_orbit_4" />
-      <S.Icon src={VolunteerActivismGreen} animationName="outer_orbit_5" />
-      <S.Icon src={SparklesYellow} animationName="outer_orbit_6" />
-      <S.Icon src={VolunteerActivismPink} animationName="outer_orbit_7" />
-      <S.Icon src={SparklesGreen} animationName="outer_orbit_8" />
-      <S.Icon src={FlareYellow} animationName="outer_orbit_9" />
+      <S.Icon position={0} src={VolunteerActivismPink} isStatic={staticIcons} />
+      <S.Icon position={1} src={FlareGreen} isStatic={staticIcons} />
+      <S.Icon
+        position={2}
+        src={VolunteerActivismYellow}
+        isStatic={staticIcons}
+      />
+      <S.Icon position={3} src={SparklesPink} isStatic={staticIcons} />
+      <S.Icon
+        position={4}
+        src={VolunteerActivismGreen}
+        isStatic={staticIcons}
+      />
+      <S.Icon position={5} src={SparklesYellow} isStatic={staticIcons} />
+      <S.Icon position={6} src={VolunteerActivismPink} isStatic={staticIcons} />
+      <S.Icon position={7} src={SparklesGreen} isStatic={staticIcons} />
+      <S.Icon position={8} src={FlareYellow} isStatic={staticIcons} />
       <S.Diamond backgroundImage={imageSrc} />
     </S.Container>
   );

82751f4aec2e84e74499faeae8686df1300236e4
Nicholas Marques <nnmarques97@gmail.com>
2022-11-09 09:40:02 -0300

    test: update CryptoPage tests

--- src/pages/promoters/SupportCausePage/CryptoPage/SelectCryptoOfferSection/index.test.tsx
+++ src/pages/promoters/SupportCausePage/CryptoPage/SelectCryptoOfferSection/index.test.tsx
@@ -1,7 +1,7 @@
 import { renderComponent } from "config/testUtils";
-import { expectTextToBeInTheDocument } from "config/testUtils/expects";
 import causeFactory from "config/testUtils/factories/causeFactory";
 import offerFactory from "config/testUtils/factories/offerFactory";
+import { screen } from "@testing-library/react";
 import OfferSelectionSection from "./index";
 
 const mockOffer = offerFactory();
@@ -19,10 +19,13 @@ describe("OfferSelectionSection", () => {
   beforeEach(() => {
     renderComponent(
       <OfferSelectionSection cause={causeFactory()} onValueChange={mockFn} />,
+      { cryptoPaymentProviderValue: { amount: "5" } },
     );
   });
 
-  it("show the first offer", () => {
-    expectTextToBeInTheDocument("5");
+  it("show the amount selected", () => {
+    expect(screen.getByRole("textbox", { name: "value-input" })).toHaveValue(
+      "5",
+    );
   });
 });

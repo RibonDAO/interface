ad4d6ff8993e6bbeb089f1c18a51e293de8f5a32
Clara Mendes <claraplanejamento@gmail.com>
2022-11-10 15:33:45 -0300

    feat: add translations

--- src/pages/donations/DonationDoneCausePage/index.tsx
+++ src/pages/donations/DonationDoneCausePage/index.tsx
@@ -1,24 +1,29 @@
 import IconsAroundImage from "components/atomics/sections/IconsAroundImage";
+import { useTranslation } from "react-i18next";
 import theme from "styles/theme";
 import * as S from "./styles";
 
 function DonationDoneCausePage(): JSX.Element {
   const { orange20 } = theme.colors;
+  const { t } = useTranslation("translation", {
+    keyPrefix: "donations.donationDoneCausePage",
+  });
 
   return (
     <S.Container>
       <S.ImageContainer>
-        <IconsAroundImage imageSrc="https://s3-alpha-sig.figma.com/img/c687/d01a/845c6c63d056be82c2a954b18c6f35f1?Expires=1668988800&Signature=hgrsNxOok2bDH0Vp-RX-b1vl6XIjQeQ4wEO2vjTFOoD28Tyl6xPPAD6GGsWTCoNqq5HMXwcQS4GMlVC5jTN882qvvJlp96538GU3r-YfltZ7J2aizTqxkfn5Anlx8SRF9COMnYrAtspy-o~pRLs4b0onKslxpYjpbomQeRSKmyTRKJPbyiUCSNiNVtjbQ7HnIgDIPKLB1jY1HR68ugGd~6YpPb1CeNaLAQ-n2gTLPTURY9FA9XglGt03cIHvJW0XDlCMRDx8Ap46SX8-6ypskkqEEoVp3PtiPyNrQhBB6oQY72xlIpSHttz-wCS8931LDB4G55z-BF4hHAY9CXA2VA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"/>
+        <IconsAroundImage imageSrc="https://s3-alpha-sig.figma.com/img/c687/d01a/845c6c63d056be82c2a954b18c6f35f1?Expires=1668988800&Signature=hgrsNxOok2bDH0Vp-RX-b1vl6XIjQeQ4wEO2vjTFOoD28Tyl6xPPAD6GGsWTCoNqq5HMXwcQS4GMlVC5jTN882qvvJlp96538GU3r-YfltZ7J2aizTqxkfn5Anlx8SRF9COMnYrAtspy-o~pRLs4b0onKslxpYjpbomQeRSKmyTRKJPbyiUCSNiNVtjbQ7HnIgDIPKLB1jY1HR68ugGd~6YpPb1CeNaLAQ-n2gTLPTURY9FA9XglGt03cIHvJW0XDlCMRDx8Ap46SX8-6ypskkqEEoVp3PtiPyNrQhBB6oQY72xlIpSHttz-wCS8931LDB4G55z-BF4hHAY9CXA2VA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" />
       </S.ImageContainer>
 
       <S.DonationValue>R$ 10</S.DonationValue>
       <S.PostDonationText>
-        Congratulations! <br /> You donated to
+        {t("title")} <br />
+        {t("titleSecondLine")}
         <S.CauseName> Education</S.CauseName>
       </S.PostDonationText>
 
       <S.FinishButton
-        text="Finish"
+        text={t("button")}
         onClick={() => {
           console.log("disparou");
         }}

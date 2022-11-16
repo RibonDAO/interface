e50e18ede3af7654e12313bbe344ff72a41a42bd
Clara Mendes <claraplanejamento@gmail.com>
2022-11-16 17:18:28 -0300

    feat: adds getOffer request

--- src/pages/donations/DonationDoneCausePage/index.tsx
+++ src/pages/donations/DonationDoneCausePage/index.tsx
@@ -1,11 +1,14 @@
 import IconsAroundImage from "components/atomics/sections/IconsAroundImage";
 import useCauses from "hooks/apiHooks/useCauses";
+import useOffers from "hooks/apiHooks/useOffers";
 import { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom";
 import { logError } from "services/crashReport";
 import theme from "styles/theme";
 import Cause from "types/entities/Cause";
+import Offer from "types/entities/Offer";
+import { Currencies } from "types/enums/Currencies";
 import * as S from "./styles";
 
 function DonationDoneCausePage(): JSX.Element {
@@ -17,35 +20,40 @@ function DonationDoneCausePage(): JSX.Element {
     offerId: number;
     causeId: number;
   };
+  const currency = Currencies.USD;
   const {
     state: { offerId, causeId },
   } = useLocation<LocationState>();
-  const { showCause } = useCauses();
+  const { getCause } = useCauses();
+  const { getOffer } = useOffers(currency);
   const [cause, setCause] = useState<Cause>();
+  const [offer, setOffer] = useState<Offer>();
 
   const donationInfos = useCallback(
-    async (id: number) => {
+    async (idCause: number, idOffer: number) => {
       try {
-        const name = await showCause(id);
+        const name = await getCause(idCause);
+        const price = await getOffer(idOffer);
         setCause(name);
+        setOffer(price);
       } catch (e) {
         logError(e);
       }
     },
-    [showCause],
+    [],
   );
 
   useEffect(() => {
-    donationInfos(causeId);
+    donationInfos(causeId, offerId);
   }, []);
 
   return (
     <S.Container>
       <S.ImageContainer>
-        <IconsAroundImage imageSrc="ht  tps://s3-alpha-sig.figma.com/img/c687/d01a/845c6c63d056be82c2a954b18c6f35f1?Expires=1668988800&Signature=hgrsNxOok2bDH0Vp-RX-b1vl6XIjQeQ4wEO2vjTFOoD28Tyl6xPPAD6GGsWTCoNqq5HMXwcQS4GMlVC5jTN882qvvJlp96538GU3r-YfltZ7J2aizTqxkfn5Anlx8SRF9COMnYrAtspy-o~pRLs4b0onKslxpYjpbomQeRSKmyTRKJPbyiUCSNiNVtjbQ7HnIgDIPKLB1jY1HR68ugGd~6YpPb1CeNaLAQ-n2gTLPTURY9FA9XglGt03cIHvJW0XDlCMRDx8Ap46SX8-6ypskkqEEoVp3PtiPyNrQhBB6oQY72xlIpSHttz-wCS8931LDB4G55z-BF4hHAY9CXA2VA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" />
+        <IconsAroundImage imageSrc={cause?.mainImage} />
       </S.ImageContainer>
 
-      <S.DonationValue>{offerId}</S.DonationValue>
+      <S.DonationValue>{offer?.price}</S.DonationValue>
       <S.PostDonationText>
         {t("title")} <br />
         {t("titleSecondLine")}

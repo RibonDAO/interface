cd8b9302167fe0432c47ad5ef4dbbee5f4022fc0
Henrique Barbosa <henrikleod@gmail.com>
2022-11-24 09:10:14 -0300

    feat: navigation menu components

---
+++ src/config/routes/Navigation/NavigationMenus/FloatingSideMenu/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Menu = styled.div<{ visible: boolean }>`
+  padding-block: 10px;
+  width: ${({ visible }) => (visible ? "150px" : "0px")};
+  border: solid 1px
+    ${({ theme, visible }) =>
+      visible ? theme.colors.defaultShadow : "transparent"};
+  border-left: none;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+  position: absolute;
+  left: 80px;
+  display: flex;
+  flex-direction: column;
+  align-self: flex-end;
+  justify-content: space-around;
+  overflow-x: hidden;
+  white-space: nowrap;
+  background: ${({ theme }) => theme.colors.neutral10};
+  box-shadow: inset 7px 0 9px -7px ${({ theme }) => theme.colors.defaultShadow};
+  transition: width 0.2s;
+`;
+
+export const MenuItem = styled(Link)`
+  padding-inline: 10px;
+  padding: 8px 0;
+  display: flex;
+  flex-direction: column;
+  font-weight: ${({ theme }) => theme.font.semibold};
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.gray30};
+
+  :hover {
+    background-color: ${({ theme }) => theme.colors.green30};
+    color: ${({ theme }) => theme.colors.neutral10};
+    box-shadow: inset 7px 0 9px -7px ${({ theme }) => theme.colors.defaultShadow};
+  }
+`;

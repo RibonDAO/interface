b01f532847ab3b9edb315c2b4a122fc21ce0ce9c
Henrique Barbosa <henrikleod@gmail.com>
2022-12-12 09:57:39 -0300

    feat: change slider on non profits list

--- src/pages/donations/CausesPage/NonProfitsList/index.tsx
+++ src/pages/donations/CausesPage/NonProfitsList/index.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 import { logEvent } from "services/analytics";
 import NonProfit from "types/entities/NonProfit";
 import Integration from "types/entities/Integration";
-import SliderCards from "components/moleculars/sliders/SliderCards";
+import SliderCardsEnhanced from "components/moleculars/sliders/SliderCardsEnhanced";
 import { useBlockedDonationModal } from "hooks/modalHooks/useBlockedDonationModal";
 import { useLocation } from "react-router-dom";
 import useVoucher from "hooks/useVoucher";
@@ -25,6 +25,8 @@ type Props = {
   setChosenNonProfit: (nonProfit: NonProfit) => void;
   setConfirmModalVisible: (visible: boolean) => void;
   canDonate: boolean;
+  currentNonProfit: number;
+  onCurrentNonProfitChange: (index: number) => void;
 };
 
 function NonProfitsList({
@@ -33,6 +35,8 @@ function NonProfitsList({
   setConfirmModalVisible,
   canDonate,
   integration,
+  currentNonProfit,
+  onCurrentNonProfitChange,
 }: Props): JSX.Element {
   const { state } = useLocation<LocationStateType>();
 
@@ -100,28 +104,36 @@ function NonProfitsList({
 
   return (
     <S.NonProfitsListContainer>
-      <SliderCards scrollOffset={400}>
-        {nonProfits.map((nonProfit, idx) => (
-          <S.CausesCardContainer key={idx.toString()}>
-            <CardCenterImageButton
-              image={nonProfit.mainImage}
-              title={`${t("impactPrefix")} ${nonProfit.impactByTicket} ${
-                nonProfit.impactDescription
-              }`}
-              buttonText={
-                canDonateAndHasVoucher
-                  ? t("donateText")
-                  : t("donateBlockedText")
-              }
-              onClickButton={() => handleButtonClick(nonProfit)}
-              onClickImage={() => handleImageClick(nonProfit)}
-              softDisabled={!canDonateAndHasVoucher}
-              infoTextLeft={nonProfit.name}
-              infoTextRight={nonProfit.cause?.name}
-            />
-          </S.CausesCardContainer>
+      <SliderCardsEnhanced
+        currentSlide={currentNonProfit}
+        onCurrentSlideChange={onCurrentNonProfitChange}
+        saveStateIdentifier="nonProfitsList"
+        loop
+      >
+        {nonProfits.map((nonProfit: any, idx: number) => (
+          <div className="card-slider__slide" key={idx.toString()}>
+            <S.CardWrapper>
+              <CardCenterImageButton
+                image={nonProfit.mainImage || nonProfit.cause?.mainImage}
+                title={`${t("impactPrefix")} ${nonProfit.impactByTicket} ${
+                  nonProfit.impactDescription
+                }`}
+                buttonText={
+                  canDonateAndHasVoucher
+                    ? t("donateText")
+                    : t("donateBlockedText")
+                }
+                onClickButton={() => handleButtonClick(nonProfit)}
+                onClickImage={() => handleImageClick(nonProfit)}
+                softDisabled={!canDonateAndHasVoucher}
+                infoTextLeft={nonProfit.name}
+                infoTextRight={nonProfit.cause?.name}
+                fullWidth
+              />
+            </S.CardWrapper>
+          </div>
         ))}
-      </SliderCards>
+      </SliderCardsEnhanced>
     </S.NonProfitsListContainer>
   );
 }

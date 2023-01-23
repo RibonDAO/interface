5c8e972dd07c9d43d7b3000ad110bbde6c33149d
mendesmcmg <claraplanejamento@gmail.com>
2023-01-23 17:58:53 -0300

    feat: adds brand colors

---
+++ src/styles/colors/brand.ts
@@ -0,0 +1,58 @@
+interface ThemeType {
+  [key: string]: any;
+}
+
+export const brandColors: ThemeType = {
+  primary: {
+    colorBrandPrimary25: "#F6FEF9",
+    colorBrandPrimary50: "#E5FBED",
+    colorBrandPrimary100: "#C5F7D5",
+    colorBrandPrimary200: "#6DDFA6",
+    colorBrandPrimary300: "#00DA93",
+    colorBrandPrimary400: "#02B67A",
+    colorBrandPrimary500: "#0EA472",
+    colorBrandPrimary600: "#1D8158",
+    colorBrandPrimary700: "#086E43",
+    colorBrandPrimary800: "#025F39",
+    colorBrandPrimary900: "#025F39",
+  },
+  secondary: {
+    colorBrandSecondary25: "#FFF7F0",
+    colorBrandSecondary50: "##FFF1E5",
+    colorBrandSecondary100: "#FFE7D6",
+    colorBrandSecondary200: "#FFCAA5",
+    colorBrandSecondary300: "#FFA86D",
+    colorBrandSecondary400: "#FA7203",
+    colorBrandSecondary500: "#E96107",
+    colorBrandSecondary600: "#C94F0D",
+    colorBrandSecondary700: "#A83B00",
+    colorBrandSecondary800: "#943100",
+    colorBrandSecondary900: "#802600",
+  },
+  tertiary: {
+    colorBrandTertiary25: "#FDF7FC",
+    colorBrandTertiary50: "#FFE7FB",
+    colorBrandTertiary100: "#FFD6E5",
+    colorBrandTertiary200: "#FBB7CF",
+    colorBrandTertiary300: "#FF8FA9",
+    colorBrandTertiary400: "#FF6B6F",
+    colorBrandTertiary500: "#E65C6C",
+    colorBrandTertiary600: "#C0415B",
+    colorBrandTertiary700: "#AA314B",
+    colorBrandTertiary800: "#9A233B",
+    colorBrandTertiary900: "#8B183B",
+  },
+  quaternary: {
+    colorBrandQuaternary25: "#FFFBEB",
+    colorBrandQuaternary50: "#FFF6CC",
+    colorBrandQuaternary100: "#FFEDA5",
+    colorBrandQuaternary200: "#FFCE00",
+    colorBrandQuaternary300: "#F5B300",
+    colorBrandQuaternary400: "#E09F00",
+    colorBrandQuaternary500: "#C28100",
+    colorBrandQuaternary600: "#A36A00",
+    colorBrandQuaternary700: "#855800",
+    colorBrandQuaternary800: "#754B00",
+    colorBrandQuaternary900: "#613E00",
+  },
+};  

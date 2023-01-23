8419d783f44b67254a4ce777659bf2ad377fc5af
mendesmcmg <claraplanejamento@gmail.com>
2023-01-23 18:10:32 -0300

    feat: adds feedback colors

---
+++ src/styles/colors/feedback.ts
@@ -0,0 +1,45 @@
+interface ThemeType {
+  [key: string]: any;
+}
+
+export const feedbackColors: ThemeType = {
+  success: {
+    colorFeedbackSuccess25: "#F6FEF9",
+    colorFeedbackSuccess50: "#E5FBED",
+    colorFeedbackSuccess100: "#C5F7D5",
+    colorFeedbackSuccess200: "#6DDFA6",
+    colorFeedbackSuccess300: "#00DA93",
+    colorFeedbackSuccess400: "#02B67A",
+    colorFeedbackSuccess500: "#0EA472",
+    colorFeedbackSuccess600: "#1D8158",
+    colorFeedbackSuccess700: "#086E43",
+    colorFeedbackSuccess800: "#025F39",
+    colorFeedbackSuccess900: "#025F39",
+  },
+  warning: {
+    colorFeedbackWarning25: "#FFFBEB",
+    colorFeedbackWarning50: "#FFF6CC",
+    colorFeedbackWarning100: "#FFEDA5",
+    colorFeedbackWarning200: "#FFCE00",
+    colorFeedbackWarning300: "#F5B300",
+    colorFeedbackWarning400: "#E09F00",
+    colorFeedbackWarning500: "#C28100",
+    colorFeedbackWarning600: "#A36A00",
+    colorFeedbackWarning700: "#855800",
+    colorFeedbackWarning800: "#754B00",
+    colorFeedbackWarning900: "#613E00",
+  },
+  error: {
+    colorFeedbackError25: "#FFFBFA",
+    colorFeedbackError50: "#FFE8E6",
+    colorFeedbackError100: "#FFDAD6",
+    colorFeedbackError200: "#FFBDB7",
+    colorFeedbackError300: "#FF968C",
+    colorFeedbackError400: "#FF695C",
+    colorFeedbackError500: "#E74132",
+    colorFeedbackError600: "#D52F20",
+    colorFeedbackError700: "#C2190A",
+    colorFeedbackError800: "#B11002",
+    colorFeedbackError900: "#9E0D00",
+  },
+};

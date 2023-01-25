d3c02c24b94cb8aaf2e3ae8e06c113e961a34b92
mendesmcmg <claraplanejamento@gmail.com>
2023-01-25 17:31:27 -0300

    chore: leaves comment

--- src/services/index.ts
+++ src/services/index.ts
@@ -30,6 +30,7 @@ export function initializeSentry(): void {
 
 export function initializeAmplitude(): any {
   const key = process.env.REACT_APP_AMPLITUDE_API_KEY;
+  // leaving this here only until I finish the dashboard
   // if (!key || process.env.NODE_ENV === "development") return;
   if (!key) return;
 

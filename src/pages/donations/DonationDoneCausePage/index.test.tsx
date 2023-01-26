a913b9e5324883e49a3a426ae808254c1e53cb98
Nathiely Macedo <nathielymm@gmail.com>
2023-01-26 17:33:45 -0300

    fix: adjust donation done tests adding nonprofitimpacts

--- src/pages/donations/DonationDoneCausePage/index.test.tsx
+++ src/pages/donations/DonationDoneCausePage/index.test.tsx
@@ -16,11 +16,22 @@ describe("DonationDoneCausePage", () => {
         nonProfit: nonProfitFactory({
           impactByTicket: 2,
           impactDescription: "days of impact",
+          nonProfitImpacts: [
+            {
+              id: 1,
+              startDate: "2021-01-01",
+              endDate: "2021-01-02",
+              usdCentsToOneImpactUnit: "100",
+              measurementUnit: "day",
+              donorRecipient: "donor",
+              impactDescription: "days of impact",
+            },
+          ],
         }),
       },
     });
 
     expectTextToBeInTheDocument("You donated");
-    expectTextToBeInTheDocument("2 days of impact");
+    expectTextToBeInTheDocument("2 days of impact for 1 donor");
   });
 });

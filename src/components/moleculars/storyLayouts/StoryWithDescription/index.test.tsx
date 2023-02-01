b11c582cfbaf3419e99e8d76404c4b8e52c05084
Henrique Barbosa <henrikleod@gmail.com>
2023-02-01 15:14:21 -0300

    fix: test without expect

--- src/components/moleculars/storyLayouts/StoryWithDescription/index.test.tsx
+++ src/components/moleculars/storyLayouts/StoryWithDescription/index.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { renderComponent } from "config/testUtils";
 import storyFactory from "config/testUtils/factories/storyFactory";
+import { screen } from "@testing-library/react";
 import StoriesWithDescription from ".";
 
 describe("StoriesWithDescription", () => {
@@ -12,5 +13,7 @@ describe("StoriesWithDescription", () => {
     renderComponent(
       <StoriesWithDescription story={story} hasProfileData={false} />,
     );
+
+    expect(screen.queryAllByTestId("spinner")).toHaveLength(1);
   });
 });

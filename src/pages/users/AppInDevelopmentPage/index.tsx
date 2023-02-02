9f162b3327a6166ff10be3014594720f8cae1ab0
letbueno <leticia.bueno.martins@gmail.com>
2023-02-02 13:23:15 -0300

    feat: add survey link in constants

--- src/pages/users/AppInDevelopmentPage/index.tsx
+++ src/pages/users/AppInDevelopmentPage/index.tsx
@@ -3,6 +3,7 @@ import LeftArrow from "assets/icons/arrow-left-green.svg";
 import { logEvent } from "services/analytics/firebase";
 import useNavigation from "hooks/useNavigation";
 import { useLanguage } from "hooks/useLanguage";
+import { SURVEY_IN_EN, SURVEY_IN_PT_BR } from "utils/constants";
 import * as S from "./styles";
 import IllustrationMobile from "./assets/illustration-mobile.svg";
 import LeftImage from "./assets/left-image.svg";
@@ -22,9 +23,9 @@ function AppInDevelopmentPage() {
     logEvent("webDwnldSurveyBtn_click");
 
     if (currentLang === "pt-BR") {
-      return "https://docs.google.com/forms/d/e/1FAIpQLSfBpxhZIYl5QKvtlPGP3UOWpVskIRn_RD5-o1QF6b4QcO0lsA/viewform?usp=sf_link";
+      return SURVEY_IN_PT_BR;
     }
-    return "https://docs.google.com/forms/d/e/1FAIpQLSc6Z7YQh56lIxD3uh1kX8TwlbkF6c9Q4nkQ1SXdblOljzFs-w/viewform?usp=sf_link";
+    return SURVEY_IN_EN;
   };
 
   const handleGoBack = () => {
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,3 +12,9 @@ export const RIBON_API =
 export const THE_GRAPH_API =
   getLocalStorageItem(SUBGRAPH_URL) ||
   "https://api.thegraph.com/subgraphs/name/ribondao/ribonsubgraph";
+
+export const SURVEY_IN_EN =
+  "https://docs.google.com/forms/d/e/1FAIpQLSc6Z7YQh56lIxD3uh1kX8TwlbkF6c9Q4nkQ1SXdblOljzFs-w/viewform?usp=sf_link";
+
+export const SURVEY_IN_PT_BR =
+  "https://docs.google.com/forms/d/e/1FAIpQLSfBpxhZIYl5QKvtlPGP3UOWpVskIRn_RD5-o1QF6b4QcO0lsA/viewform?usp=sf_link";

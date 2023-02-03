1b6fa5d2f65247ce1c10f7c682a604aa390f3c97
Henrique Barbosa <henrikleod@gmail.com>
2023-02-03 11:40:19 -0300

    feat: add get the app item to header

--- src/layouts/LayoutHeader/UserSupportItem/styles.ts
+++ src/layouts/LayoutHeader/UserSupportItem/styles.ts
@@ -1,14 +1,7 @@
-import Button from "components/atomics/buttons/Button";
 import styled from "styled-components";
 
-export const Container = styled.div``;
-
-export const UserSupportButton = styled(Button)`
-  min-width: 76px;
-  padding: ${({ theme }) => theme.spacing(4, 12)};
-  background: ${({ theme }) => theme.colors.white};
-`;
-
-export const GoButton = styled.img`
+export const Container = styled.div`
   cursor: pointer;
 `;
+
+export const GoButton = styled.img``;

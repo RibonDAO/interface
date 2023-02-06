35b5ec2b4b7d119623882a3646122af52386979b
Henrique Barbosa <henrikleod@gmail.com>
2023-02-06 09:40:05 -0300

    fix: use nullsafe operator

--- src/pages/donations/CausesPage/NonProfitsList/index.tsx
+++ src/pages/donations/CausesPage/NonProfitsList/index.tsx
@@ -120,7 +120,7 @@ function NonProfitsList({
               softDisabled={!canDonateAndHasVoucher}
               infoTextLeft={nonProfit.name}
               infoTextRight={nonProfit.cause?.name}
-              infoText={nonProfit.stories.length ? t("learnMore") : undefined}
+              infoText={nonProfit.stories?.length ? t("learnMore") : undefined}
               fullWidth
             />
           </S.CardWrapper>

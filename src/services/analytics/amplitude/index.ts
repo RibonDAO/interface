4eafe8bbaf13d27ead8f3ec236f1e2bd2dc99bb0
Thais Correa <thais@ribon.io>
2023-02-08 16:44:52 -0300

    fix: remove comment

--- src/services/analytics/amplitude/index.ts
+++ src/services/analytics/amplitude/index.ts
@@ -4,8 +4,6 @@ import { logError } from "services/crashReport";
 
 export function initializeAmplitude(): any {
   const key = process.env.REACT_APP_AMPLITUDE_API_KEY;
-  // leaving this here only until I finish the dashboard
-  // if (!key || process.env.NODE_ENV === "development") return;
   if (!key) return;
 
   amplitude.init(key);

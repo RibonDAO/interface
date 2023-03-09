3f94102edca4b21330f92b97ece43e2028bfaf02
Caio Victor <caiovictor.pcb@gmail.com>
2023-03-09 00:28:15 -0300

    feat: create new testUtil expect function and add tests to Dropdown

--- src/components/atomics/Dropdown/index.test.tsx
+++ src/components/atomics/Dropdown/index.test.tsx
@@ -1,5 +1,9 @@
 import { clickOn, renderComponent } from "config/testUtils";
-import { expectTextToBeInTheDocument } from "config/testUtils/expects";
+import {
+  expectFunctionNotToHaveBeenCalledWith,
+  expectTextNotToBeInTheDocument,
+  expectTextToBeInTheDocument
+} from "config/testUtils/expects";
 import Dropdown from ".";
 
 describe("Dropdown", () => {
@@ -7,12 +11,12 @@ describe("Dropdown", () => {
 
   beforeEach(() => {
     renderComponent(
-      <Dropdown
-        name="dropdown"
-        values={["test1", "test2"]}
-        label="dropdown"
-        onOptionChanged={mockFn}
-      />,
+        <Dropdown
+            name="dropdown"
+            values={["test1", "test2"]}
+            label="dropdown"
+            onOptionChanged={mockFn}
+        />,
     );
   });
 
@@ -32,4 +36,28 @@ describe("Dropdown", () => {
 
     expect(mockFn).toHaveBeenCalledWith("test1");
   });
+
+  it("update optionsVisible state when onClose", () => {
+    clickOn("dropdown");
+    clickOn("test1");
+    expectTextNotToBeInTheDocument("test1")
+  });
+
+  describe("without onOptionChanged", () => {
+    const onOptionChangedMock = jest.fn();
+    it("should not call onOptionChanged when clicked", () => {
+      renderComponent(
+          <Dropdown
+              name="dropdown-without-onOptionChanged"
+              values={["option1", "option2"]}
+              label="dropdown-without-onOptionChanged"
+          />,
+      );
+      clickOn("dropdown-without-onOptionChanged");
+      clickOn("option1");
+      expectFunctionNotToHaveBeenCalledWith(onOptionChangedMock, "test1");
+    });
+  });
 });
+
+

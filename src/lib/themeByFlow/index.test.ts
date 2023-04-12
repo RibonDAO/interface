d1ae7ee747081700a480fd57573f63d3db56a9f5
Henrique Barbosa <henrikleod@gmail.com>
2023-04-12 15:06:02 -0300

    fix: broken test after refactoring

--- src/lib/themeByFlow/index.test.ts
+++ src/lib/themeByFlow/index.test.ts
@@ -1,22 +1,29 @@
 import getThemeByFlow from ".";
 
-const causeTheme = JSON.parse(
-  "{'shade10': '#FFCAA5', 'shade20': '#FFA86D', 'shade30': '#FA7203', 'shade40': '#A83B00'}",
-);
-const nonProfitTheme = JSON.parse(
-  "{'shade10': '#FFE7FB', 'shade20': '#FBB7CF', 'shade30': '#FF6B6F', 'shade40': '#9A233B'}",
-);
+const causeObject = {
+  shade10: "#FFCAA5",
+  shade20: "#FFA86D",
+  shade30: "#FA7203",
+  shade40: "#A83B00",
+};
+
+const nonProfitObject = {
+  shade10: "#FFE7FB",
+  shade20: "#FBB7CF",
+  shade30: "#FF6B6F",
+  shade40: "#9A233B",
+};
 
 describe("getThemeByFlow", () => {
   it("should return the cause theme", () => {
-    expect(getThemeByFlow("cause")).toEqual(causeTheme);
+    expect(getThemeByFlow("cause")).toEqual(causeObject);
   });
 
   it("should return the non profit theme", () => {
-    expect(getThemeByFlow("nonProfit")).toEqual(nonProfitTheme);
+    expect(getThemeByFlow("nonProfit")).toEqual(nonProfitObject);
   });
 
   it("should return the default theme", () => {
-    expect(getThemeByFlow("")).toEqual(causeTheme);
+    expect(getThemeByFlow("")).toEqual(causeObject);
   });
 });

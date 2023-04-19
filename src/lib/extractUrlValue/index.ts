26e1ffccf79491da585a90b8e280dd0328a9f487
Nicholas Marques <devnicknish@gmail.com>
2023-04-19 16:20:43 -0300

    fix: add safe operator to extractUrlValue

--- src/lib/extractUrlValue/index.ts
+++ src/lib/extractUrlValue/index.ts
@@ -2,7 +2,7 @@ export default function extractUrlValue(
   key: string,
   url: string,
 ): string | undefined {
-  const match = url.match(`[?&]${key}=([^&]+)`);
+  const match = url?.match(`[?&]${key}=([^&]+)`);
   const value = match && match[1];
   return value || undefined;
 }

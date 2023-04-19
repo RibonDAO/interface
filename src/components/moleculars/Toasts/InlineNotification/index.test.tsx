30cb8daeccfc0b5acc7df55302ace82cd797fb7d
Nicholas Marques <devnicknish@gmail.com>
2023-04-19 08:45:03 -0300

    test: update InlineNotification tests

--- src/components/moleculars/Toasts/InlineNotification/index.test.tsx
+++ src/components/moleculars/Toasts/InlineNotification/index.test.tsx
@@ -1,5 +1,8 @@
-import { renderComponent } from "config/testUtils";
-import { expectTextToBeInTheDocument } from "config/testUtils/expects";
+import { clickOn, renderComponent } from "config/testUtils";
+import {
+  expectTextNotToBeInTheDocument,
+  expectTextToBeInTheDocument,
+} from "config/testUtils/expects";
 import InlineNotification from ".";
 
 describe("InlineNotification", () => {
@@ -15,5 +18,63 @@ describe("InlineNotification", () => {
     );
 
     expectTextToBeInTheDocument("InlineNotification");
+  });
+
+  describe("when close icon is pressed", () => {
+    const onCloseClick = jest.fn();
+
+    beforeEach(() => {
+      renderComponent(
+        <InlineNotification
+          title="title"
+          description="description"
+          type="success"
+          onCloseClick={onCloseClick}
+        />,
+      );
+    });
+
+    it("hides the component", () => {
+      expectTextToBeInTheDocument("title");
+      expectTextToBeInTheDocument("description");
+
+      clickOn("close");
+
+      expectTextNotToBeInTheDocument("title");
+      expectTextNotToBeInTheDocument("description");
+    });
+
+    it("calls the onCloseClick function", () => {
+      clickOn("close");
+
+      expect(onCloseClick).toHaveBeenCalled();
+    });
+  });
+
+  describe("when the links are pressed", () => {
+    const onLink1Click = jest.fn();
+    const onLink2Click = jest.fn();
+
+    beforeEach(() => {
+      renderComponent(
+        <InlineNotification
+          title="title"
+          description="description"
+          type="success"
+          link1="link1"
+          link2="link2"
+          onLink1Click={onLink1Click}
+          onLink2Click={onLink2Click}
+        />,
+      );
+    });
+
+    it("calls the onLinkClick", () => {
+      clickOn("link1");
+      clickOn("link2");
+
+      expect(onLink1Click).toHaveBeenCalled();
+      expect(onLink2Click).toHaveBeenCalled();
+    });
   });
 });

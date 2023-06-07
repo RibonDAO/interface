63592ea06c40250dc352bd5b0ed1e97ea9e75030
Nicholas Marques <devnicknish@gmail.com>
2023-06-07 10:36:46 -0300

    test: add useLocationSearch update method tests

--- src/hooks/useLocationSearch/index.test.tsx
+++ src/hooks/useLocationSearch/index.test.tsx
@@ -1,13 +1,14 @@
 import { renderHook } from "@testing-library/react-hooks";
 import { useLocationSearch } from ".";
 
+const mockPush = jest.fn();
 jest.mock("react-router-dom", () => ({
   useLocation: () => ({
     search: "search",
     pathname: "pathname",
   }),
   useHistory: () => ({
-    push: jest.fn(),
+    push: mockPush,
   }),
 }));
 
@@ -15,5 +16,15 @@ describe("useLocationSearch", () => {
   it("should return the correct params", () => {
     const { result } = renderHook(() => useLocationSearch());
     expect(result.current.search).toEqual("search");
+  });
+
+  it("updates the location search correctly", () => {
+    const { result } = renderHook(() => useLocationSearch());
+    result.current.updateLocationSearch("key", "value");
+
+    expect(mockPush).toHaveBeenCalledWith({
+      pathname: "pathname",
+      search: "search=&key=value",
+    });
   });
 });

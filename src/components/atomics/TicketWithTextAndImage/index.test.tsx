3ce8764664d0f2f75fc799e0bf63952c380ce6c7
Nathiely Macedo <nathielymm@gmail.com>
2023-06-29 00:15:30 -0300

    test: adds some tests

--- src/components/atomics/TicketWithTextAndImage/index.test.tsx
+++ src/components/atomics/TicketWithTextAndImage/index.test.tsx
@@ -1,13 +1,14 @@
-import { renderComponent } from "config/testUtils";
+import { clickOn, renderComponent } from "config/testUtils";
 import {
   expectImageToBeInTheDocument,
   expectTextToBeInTheDocument,
 } from "config/testUtils/expects";
 import logo from "assets/icons/logo.svg";
+import { mockNewLogEventFunction } from "setupTests";
 import TicketWithTextAndImage from ".";
 
 describe("TicketWithTextAndImage", () => {
-  it("should render without error", () => {
+  it("renders with link", () => {
     renderComponent(
       <TicketWithTextAndImage
         title="title"
@@ -20,5 +21,35 @@ describe("TicketWithTextAndImage", () => {
     expectTextToBeInTheDocument("title");
     expectTextToBeInTheDocument("subtitle");
     expectImageToBeInTheDocument("logo");
+  });
+
+  it("renders without link", () => {
+    renderComponent(
+      <TicketWithTextAndImage title="title" subtitle="subtitle" image={logo} />,
+    );
+
+    expectTextToBeInTheDocument("title");
+    expectTextToBeInTheDocument("subtitle");
+    expectImageToBeInTheDocument("logo");
+  });
+
+  it("renders with link and event", () => {
+    renderComponent(
+      <TicketWithTextAndImage
+        title="title"
+        subtitle="subtitle"
+        link="https://example.com"
+        image={logo}
+        eventName="eventName"
+        eventParams={{ eventParams: "eventParams" }}
+      />,
+    );
+    window.open = jest.fn();
+
+    clickOn("subtitle");
+    expect(mockNewLogEventFunction).toHaveBeenCalledWith("click", "eventName", {
+      eventParams: "eventParams",
+    });
+    expect(window.open).toHaveBeenCalledWith("https://example.com", "_blank");
   });
 });

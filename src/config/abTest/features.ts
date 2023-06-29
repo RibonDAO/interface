a035e112288b42b92d934ab876dda1bc211d59b9
Nathiely Macedo <nathielymm@gmail.com>
2023-06-28 23:53:43 -0300

    fix: config experiment enviroment

--- src/config/abTest/features.ts
+++ src/config/abTest/features.ts
@@ -1,9 +1,3 @@
-import { useExperiment } from "@growthbook/growthbook-react";
-
-export function ImpactVariation() {
-  const { value } = useExperiment({
-    key: "impact-conversion-staging",
-    variations: ["Control", "NewImpact", "OldImpact"],
-  });
-  return value;
-}
+export const nameOfFeature = () => {
+  // useFeature("name-of-feature").on;
+};

a2f69c3114598a40ab40fd382d4718faec124b00
Nicholas Marques <devnicknish@gmail.com>
2023-07-10 09:02:16 -0300

    fix: lint css

--- src/pages/donations/ConfirmDonationPage/DonatingSection/styles.ts
+++ src/pages/donations/ConfirmDonationPage/DonatingSection/styles.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  height: calc(100vh - 112px);
   padding: ${({ theme }) => theme.spacing(0, 16)};
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: calc(100vh - 112px);
 
   @media (min-width: ${({ theme }) => theme.breakpoints.pad}) {
     height: calc(100vh - 256px);

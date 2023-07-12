b17941a9f6979512218d2973c5a9c57bb1e5a36e
Nicholas Marques <devnicknish@gmail.com>
2023-07-12 10:33:31 -0300

    test: update layout header tests

--- src/layouts/LayoutHeader/index.test.tsx
+++ src/layouts/LayoutHeader/index.test.tsx
@@ -4,7 +4,30 @@ import { setLocalStorageItem } from "lib/localStorage";
 import { HAS_AN_AVAILABLE_VOUCHER } from "lib/localStorage/constants";
 import React from "react";
 import { mockRequest } from "config/testUtils/test-helper";
+import { screen } from "@testing-library/react";
 import LayoutHeader from ".";
+
+const mockIntegration = {
+  id: "1",
+  name: "Test Integration",
+  logo: "logo",
+  integrationTask: {
+    linkAddress: "https://www.test.com",
+  },
+};
+jest.mock("@ribon.io/shared/hooks", () => ({
+  __esModule: true,
+  ...jest.requireActual("@ribon.io/shared/hooks"),
+  useIntegration: () => ({
+    integration: mockIntegration,
+    refetch: jest.fn(),
+  }),
+}));
+jest.mock("hooks/useIntegrationId", () => ({
+  __esModule: true,
+  ...jest.requireActual("hooks/useIntegrationId"),
+  useIntegrationId: () => "9",
+}));
 
 describe("LayoutHeader", () => {
   mockRequest("/api/v1/users/can_donate", {
@@ -22,5 +45,20 @@ describe("LayoutHeader", () => {
 
     clickOn("1");
     expectPageToNavigateTo("/tickets");
+  });
+
+  describe("when side logo is clicked", () => {
+    describe("when user can donate", () => {
+      beforeEach(() => {
+        renderComponent(<LayoutHeader />);
+
+        const sideLogo = screen.getByAltText("side-logo");
+        clickOn(sideLogo);
+      });
+
+      it("goes to the return to integration page", () => {
+        expectPageToNavigateTo("return-to-integration");
+      });
+    });
   });
 });

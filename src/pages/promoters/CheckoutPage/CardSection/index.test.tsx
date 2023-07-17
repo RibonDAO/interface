013dc7837f6ed4e2ca41e5605929b217e0850a00
Henrique Barbosa <henrikleod@gmail.com>
2023-07-17 14:28:08 -0300

    chore: improve card section tests

--- src/pages/promoters/CheckoutPage/CardSection/index.test.tsx
+++ src/pages/promoters/CheckoutPage/CardSection/index.test.tsx
@@ -1,11 +1,32 @@
 import { renderComponent } from "config/testUtils";
-import { screen } from "@testing-library/react";
+import { NonProfit } from "@ribon.io/shared/types";
+import { expectTextToBeInTheDocument } from "config/testUtils/expects";
 import CardSection from ".";
+
+jest.mock("hooks/usePaymentParams", () => ({
+  __esModule: true,
+  default: () => ({
+    currency: "USDC",
+    target: "non_profit",
+    targetId: 1,
+    offer: 1,
+  }),
+}));
+
+jest.mock("hooks/usePayable", () => ({
+  __esModule: true,
+  default: () => ({
+    currentPayable: {
+      id: 1,
+      name: "test",
+    } as NonProfit,
+  }),
+}));
 
 describe("CardSection", () => {
   it("should render without error", () => {
     renderComponent(<CardSection />);
 
-    expect(screen.queryAllByTestId("checkout-page-loader")).toHaveLength(1);
+    expectTextToBeInTheDocument("Payment");
   });
 });

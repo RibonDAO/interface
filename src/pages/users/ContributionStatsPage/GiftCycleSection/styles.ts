f42055de3cc886e27a02371f20c83868023aaca6
Nicholas Marques <devnicknish@gmail.com>
2023-07-17 13:39:47 -0300

    fix: lint css

--- src/pages/users/ContributionStatsPage/GiftCycleSection/styles.ts
+++ src/pages/users/ContributionStatsPage/GiftCycleSection/styles.ts
@@ -4,16 +4,16 @@ import { defaultBodyXsRegular } from "styles/typography/default";
 export const Container = styled.div``;
 
 export const GiftCard = styled.div`
+  margin-right: -16px;
   margin-bottom: ${({ theme }) => theme.spacing(40)};
+  margin-left: -16px;
   border-radius: 8px;
   background: transparent;
-  margin-left: -16px;
-  margin-right: -16px;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.pad}) {
+    margin-right: 0;
     margin-bottom: ${({ theme }) => theme.spacing(16)};
     margin-left: 0;
-    margin-right: 0;
     border-width: 1px;
     border-style: solid;
     border-color: ${({ theme }) => theme.colors.neutral[100]};
@@ -26,9 +26,9 @@ export const GiftDonationCycleImage = styled.img`
 `;
 
 export const RealTimeDonationContainer = styled.div`
+  margin-bottom: ${({ theme }) => theme.spacing(24)};
   padding: ${({ theme }) => theme.spacing(4, 8, 4, 8)};
   border-radius: 30px;
-  margin-bottom: ${({ theme }) => theme.spacing(24)};
   display: flex;
   align-items: center;
   justify-content: center;
@@ -41,7 +41,8 @@ export const RealTimeDonationContainer = styled.div`
 `;
 
 export const RealTimeDonationLabel = styled.span`
-  ${defaultBodyXsRegular};
-  color: ${({ theme }) => theme.colors.brand.primary[900]};
+  ${defaultBodyXsRegular}
+
   background-color: ${({ theme }) => theme.colors.feedback.success[50]};
+  color: ${({ theme }) => theme.colors.brand.primary[900]};
 `;

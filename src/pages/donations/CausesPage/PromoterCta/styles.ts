e29c09c69010ad3b07f10c142f88382f2f628874
Nicholas Marques <devnicknish@gmail.com>
2023-07-19 10:14:06 -0300

    fix: lint css

--- src/pages/donations/CausesPage/PromoterCta/styles.ts
+++ src/pages/donations/CausesPage/PromoterCta/styles.ts
@@ -10,21 +10,23 @@ export const OuterContainer = styled.div`
   margin-bottom: ${({ theme }) => theme.spacing(16)};
 `;
 export const Container = styled.div`
+  width: 100%;
+  padding: ${({ theme }) => theme.spacing(0, 16, 0, 16)};
   position: relative;
   display: flex;
   flex-direction: column;
-  width: 100%;
-  padding: ${({ theme }) => theme.spacing(0, 16, 0, 16)};
 `;
 
 export const Title = styled.h1`
   ${stylizedDisplayXs}
-  color: ${({ theme }) => theme.colors.neutral[25]};
+
   margin-bottom: ${({ theme }) => theme.spacing(8)};
+  color: ${({ theme }) => theme.colors.neutral[25]};
 `;
 
 export const Text = styled.span`
   ${defaultBodyMdRegular}
+
   color: ${({ theme }) => theme.colors.neutral[25]};
 `;
 
@@ -34,25 +36,26 @@ export const TextList = styled.li`
 
 export const CtaButton = styled(Button)`
   margin-top: ${({ theme }) => theme.spacing(24)};
-  color: ${({ theme }) => theme.colors.brand.primary[900]};
-  background-color: ${({ theme }) => theme.colors.brand.primary[200]};
   border-color: ${({ theme }) => theme.colors.brand.primary[200]};
+  background-color: ${({ theme }) => theme.colors.brand.primary[200]};
+  color: ${({ theme }) => theme.colors.brand.primary[900]};
 `;
 
 export const Label = styled.span`
   ${defaultBodyXsRegular}
+
   color: ${({ theme }) => theme.colors.brand.primary[900]};
 `;
 export const LabelContainer = styled.div`
-  border-radius: 30px;
   width: 46px;
   height: 28px;
-  background-color: ${({ theme }) => theme.colors.feedback.success[50]};
-  display: flex;
-  justify-content: center;
-  align-items: center;
   border-color: ${({ theme }) => theme.colors.brand.primary[900]};
+  border-radius: 30px;
   position: absolute;
   top: 0;
   right: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.feedback.success[50]};
 `;

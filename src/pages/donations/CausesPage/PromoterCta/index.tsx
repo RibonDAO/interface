b36a8a0ec9c168d750250d65f73347decc0913b9
Nicholas Marques <devnicknish@gmail.com>
2023-07-25 08:46:15 -0300

    feat: add navigate link to promoter cta

--- src/pages/donations/CausesPage/PromoterCta/index.tsx
+++ src/pages/donations/CausesPage/PromoterCta/index.tsx
@@ -1,17 +1,36 @@
 import Banner from "components/moleculars/cards/Banner";
 import Background from "assets/images/background-green-shapes.svg";
 import { useTranslation } from "react-i18next";
-import { newLogEvent } from "lib/events";
+import { logEvent } from "lib/events";
 import { theme } from "@ribon.io/shared/styles";
+import { useExperiment } from "@growthbook/growthbook-react";
+import useNavigation from "hooks/useNavigation";
 import * as S from "./styles";
 
 function PromoterCta() {
   const { t } = useTranslation("translation", {
     keyPrefix: "donations.causesPage.promoterCta",
   });
+  const { value: variation } = useExperiment({
+    key: "support-cause-page-links",
+    variations: ["Control", "SupportDash", "SupportCause"],
+  });
+  const { navigateTo } = useNavigation();
+
+  const navigateLink = () => {
+    if (variation === "SupportCause") return "/promoters/support-cause-flow";
+    if (variation === "SupportDash")
+      return "/promoters/support-cause-dash-flow";
+
+    return "/promoters/support-cause";
+  };
 
   const handleButtonClick = () => {
-    newLogEvent("click", "promoterCta");
+    logEvent("giveCauseCard_click", {
+      from: "promoterBannerCta",
+    });
+
+    navigateTo(navigateLink());
   };
 
   return (

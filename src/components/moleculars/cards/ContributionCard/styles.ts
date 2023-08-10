bd606dbd7abc3a4f441607f9eda0ba1b0dabc1f9
Henrique Barbosa <henrikleod@gmail.com>
2023-08-10 17:34:11 -0300

    feat: progression ab testing for impression cards

--- src/components/moleculars/cards/ContributionCard/styles.ts
+++ src/components/moleculars/cards/ContributionCard/styles.ts
@@ -1,27 +1,29 @@
 import styled from "styled-components";
 import {
   defaultBodyMdRegular,
+  defaultBodyMdSemibold,
   defaultBodyXsBold,
+  defaultBodyXsRegular,
 } from "styles/typography/default";
 import { stylizedDisplayXs } from "styles/typography/stylized";
 import Button from "components/atomics/buttons/Button";
 
-export const Container = styled.div`
+export const Container = styled.div<{ colorTheme: any }>`
   max-width: 458px;
   padding: ${({ theme }) => theme.spacing(32)};
   border-radius: 8px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  background-color: ${({ theme }) => theme.colors.brand.primary[50]};
+  background-color: ${({ colorTheme }) => colorTheme[50]};
 `;
 
-export const Title = styled.h1`
+export const Title = styled.h1<{ colorTheme: any }>`
   ${defaultBodyXsBold}
 
   margin-bottom: ${({ theme }) => theme.spacing(8)};
   text-transform: uppercase;
-  color: ${({ theme }) => theme.colors.brand.primary[500]};
+  color: ${({ colorTheme }) => colorTheme[500]};
 `;
 
 export const Description = styled.p`
@@ -35,19 +37,48 @@ export const Description = styled.p`
   }
 `;
 
-export const DonationButton = styled(Button)`
+export const DonationButton = styled(Button)<{ colorTheme: any }>`
   width: 100%;
   height: 48px;
   padding: ${({ theme }) => theme.spacing(12, 16)};
   border: none;
   border-radius: 4px;
-  background-color: ${({ theme }) => theme.colors.brand.primary[600]};
+  background-color: ${({ colorTheme }) => colorTheme[600]};
   color: ${({ theme }) => theme.colors.neutral[50]};
 `;
 
-export const Value = styled.span`
+export const Value = styled.span<{ colorTheme: any }>`
   ${stylizedDisplayXs}
 
   margin-bottom: ${({ theme }) => theme.spacing(4)};
-  color: ${({ theme }) => theme.colors.brand.primary[900]};
+  color: ${({ colorTheme }) => colorTheme[900]};
+`;
+
+export const Centered = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LifeAmount = styled.h4`
+  ${defaultBodyMdSemibold}
+
+  margin-block: ${({ theme }) => theme.spacing(4)};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.brand.tertiary[800]};
+`;
+
+export const HeartIcon = styled.img`
+  margin-right: ${({ theme }) => theme.spacing(4)};
+`;
+
+export const ImpactDescription = styled.p`
+  ${defaultBodyXsRegular}
+
+  margin-bottom: ${({ theme }) => theme.spacing(16)};
+  color: ${({ theme }) => theme.colors.neutral[500]};
 `;

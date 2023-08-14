5c62c0548932591162be11dae55dceacd41948fe
Clara Mendes <claraplanejamento@gmail.com>
2023-08-14 09:29:20 -0300

    feat: adds monthly contributions page

---
+++ src/pages/promoters/MonthlyContributionsPage/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+function MonthlyContributionsPage(): JSX.Element {
+  return (
+    <S.Container>
+      <h1>MonthlyContributionsPage</h1>
+    </S.Container>
+  );
+}
+
+export default MonthlyContributionsPage;

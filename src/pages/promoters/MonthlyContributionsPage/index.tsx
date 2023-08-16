81309a1120112b74d264727c0fd39bf6112d2f00
Clara Mendes <claraplanejamento@gmail.com>
2023-08-16 11:53:06 -0300

    feat: adds monthly contributions page basic layout

--- src/pages/promoters/MonthlyContributionsPage/index.tsx
+++ src/pages/promoters/MonthlyContributionsPage/index.tsx
@@ -1,11 +1,20 @@
+import ArrowLeft from "assets/icons/arrow-left-green.svg";
+import useNavigation from "hooks/useNavigation";
+import { useTranslation } from "react-i18next";
 import * as S from "./styles";
 
-function MonthlyContributionsPage(): JSX.Element {
+function MonthlyContributionPage(): JSX.Element {
+  const { navigateBack } = useNavigation();
+  const { t } = useTranslation("translation", {
+    keyPrefix: "promoters.monthlyContributionsPage",
+  });
+
   return (
     <S.Container>
-      <h1>MonthlyContributionsPage</h1>
+      <S.BackArrowButton src={ArrowLeft} onClick={navigateBack} />
+      <S.Title>{t("title")}</S.Title>
     </S.Container>
   );
 }
 
-export default MonthlyContributionsPage;
+export default MonthlyContributionPage;
--- src/pages/promoters/MonthlyContributionsPage/styles.ts
+++ src/pages/promoters/MonthlyContributionsPage/styles.ts
@@ -1,5 +1,26 @@
 import styled from "styled-components";
+import { stylizedDisplayLg } from "styles/typography/stylized";
 
 export const Container = styled.div`
+  padding: ${({ theme }) => theme.spacing(16)};
+  flex-direction: column;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.neutral10};
+`;
+
+export const Title = styled.h1`
+  ${stylizedDisplayLg}
+`;
+
+export const Image = styled.img`
   width: 100%;
+  height: 100%;
+  margin-top: ${({ theme }) => theme.spacing(64)};
+`;
+
+export const BackArrowButton = styled.img`
+  width: 32px;
+  height: 32px;
+  margin-bottom: ${({ theme }) => theme.spacing(20)};
+  cursor: pointer;
 `;

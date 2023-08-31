a543c2b9819aad7344222c1c9931fe803ff426ba
mendesmcmg <claraplanejamento@gmail.com>
2023-08-31 09:38:48 -0300

    test: adds english test case

--- src/components/moleculars/banners/UserSupportBanner/index.test.tsx
+++ src/components/moleculars/banners/UserSupportBanner/index.test.tsx
@@ -3,8 +3,16 @@ import {
   expectLogEventToHaveBeenCalledWith,
   expectTextToBeInTheDocument,
 } from "config/testUtils/expects";
+import { setLocalStorageItem } from "lib/localStorage";
 import { screen } from "@testing-library/react";
+import startSupportChat from "services/support";
+import { I18NEXTLNG } from "lib/currentLanguage";
 import UserSupportBanner from ".";
+
+jest.mock("services/support", () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
 
 describe("UserSupportBanner", () => {
   window.open = jest.fn();
@@ -16,14 +24,19 @@ describe("UserSupportBanner", () => {
     expectTextToBeInTheDocument("Questions, ideias, refunds. We’re here!");
   });
 
-  describe("when the banner is clicked", () => {
+  describe("when the banner is clicked in portuguese", () => {
     beforeEach(() => {
+      Object.defineProperty(window, "navigator", {
+        value: { language: "pt-BR" },
+      });
+      setLocalStorageItem(I18NEXTLNG, "pt-BR");
+      
       renderComponent(<UserSupportBanner from="test" />);
       const banner = screen.getByTestId("banner");
-
+      
       clickOn(banner);
     });
-
+    
     it("opens the support link", () => {
       expect(window.open).toHaveBeenCalled();
     });
@@ -32,4 +45,23 @@ describe("UserSupportBanner", () => {
       expectLogEventToHaveBeenCalledWith("supportBtn_click", { from: "test" });
     });
   });
+
+  describe("when the banner is clicked in English", () => {
+    beforeEach(() => {
+      Object.defineProperty(window, "navigator", {
+        value: { language: "en" },
+      });
+      setLocalStorageItem(I18NEXTLNG, "en");
+  
+      renderComponent(<UserSupportBanner from="test" />);
+      const banner = screen.getByTestId("banner");
+  
+      clickOn(banner);
+    });
+  
+    it("starts the support chat", () => {
+      expect(startSupportChat).toHaveBeenCalled();
+    });
+  });
+  
 });

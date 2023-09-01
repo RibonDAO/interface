bc278354f3c3dcd8458c6884956df9dde3b33994
Henrique Barbosa <henrikleod@gmail.com>
2023-09-01 12:00:21 -0300

    chore: about iframe

---
+++ src/pages/promoters/AboutPage/index.tsx
@@ -0,0 +1,9 @@
+import * as S from "./styles";
+
+function AboutPage(): JSX.Element {
+  return (
+    <S.Container src="https://projetos.ribon.io/sobre" title="Sobre a Ribon" />
+  );
+}
+
+export default AboutPage;

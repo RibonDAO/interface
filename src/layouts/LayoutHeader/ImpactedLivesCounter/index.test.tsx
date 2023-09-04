439410e7c5e75d5616c4a56841c7f2271286dade
Nicholas Marques <devnicknish@gmail.com>
2023-09-04 15:12:27 -0300

    test: fix ImpactedLivesCounter test with new context

--- src/layouts/LayoutHeader/ImpactedLivesCounter/index.test.tsx
+++ src/layouts/LayoutHeader/ImpactedLivesCounter/index.test.tsx
@@ -2,13 +2,13 @@ import { renderComponent } from "config/testUtils/renders";
 import { expectTextToBeInTheDocument } from "config/testUtils/expects";
 import ImpactedLivesCounter from ".";
 
-jest.mock("hooks/useUserLevel", () => () => ({
-  userExperience: 100,
-}));
-
 describe("ImpactedLivesCounter", () => {
   it("renders the amount of impacted lives by the user", () => {
-    renderComponent(<ImpactedLivesCounter />);
+    renderComponent(<ImpactedLivesCounter />, {
+      userLevelProviderValue: {
+        userExperience: 100,
+      },
+    });
 
     expectTextToBeInTheDocument("100");
   });

083475e27a2bd1c99e53c3f19c61e8a49621f873
Nicholas Marques <devnicknish@gmail.com>
2023-09-05 15:29:24 -0300

    test: add useUserBadges test

---
+++ src/hooks/useUserBadges/index.test.tsx
@@ -0,0 +1,22 @@
+import { renderHook } from "config/testUtils/renders";
+import useUserBadges from ".";
+
+describe("useUserBadges", () => {
+  it("should return the user badges", () => {
+    const { hook } = renderHook(() => useUserBadges(), {
+      userLevelProviderValue: {
+        userLevel: 5,
+      },
+    });
+
+    expect(hook?.result.current.badges[0]).toEqual({
+      id: 1,
+      name: "Level 5",
+      description:
+        "Congratulations! You have now impacted 21 lives with your donations and unlocked the achievement ‘Level 5’.",
+      image: "https://i.imgur.com/wv66Hlj.png",
+      category: "level",
+      achieved: true,
+    });
+  });
+});

e77adfe5a41fe538939ef2209272b6f481bb57f9
Thais Correa <thais@ribon.io>
2023-09-14 16:54:27 -0300

    fix: animation-block

--- src/hooks/useDonationFlow/index.tsx
+++ src/hooks/useDonationFlow/index.tsx
@@ -15,6 +15,7 @@ import useNavigation from "hooks/useNavigation";
 import useVoucher from "hooks/useVoucher";
 import { normalizedLanguage } from "lib/currentLanguage";
 import { getUTMFromLocationSearch } from "lib/getUTMFromLocationSearch";
+import { useTranslation } from "react-i18next";
 
 type HandleDonateProps = {
   nonProfit: NonProfit;
@@ -31,6 +32,10 @@ function useDonationFlow() {
   const { integration } = useIntegration(integrationId);
   const { history, navigateTo } = useNavigation();
   const { destroyVoucher } = useVoucher();
+
+  const { t } = useTranslation("translation", {
+    keyPrefix: "donations.causesPage",
+  });
 
   function getExternalIdFromLocationSearch() {
     return extractUrlValue("external_id", history.location.search);
@@ -50,34 +55,33 @@ function useDonationFlow() {
 
     const utmParams = getUTMFromLocationSearch(history.location.search);
 
-    if (integration) {
-      try {
-        await donate(
-          integration?.id,
-          nonProfit.id,
-          email,
-          PLATFORM,
-          getExternalIdFromLocationSearch(),
-          utmParams.utmSource,
-          utmParams.utmMedium,
-          utmParams.utmCampaign,
-        );
-        destroyVoucher();
-        if (onSuccess) onSuccess();
-      } catch (e: any) {
-        logError(e);
-        if (onError) onError(e);
-        const failedKey =
-          e.response.status === 403 ? "blockedDonation" : "failedDonation";
-        const newState = {
-          [failedKey]: true,
-          message: e.response.data?.formatted_message,
-        };
-        navigateTo({ pathname: "/causes", state: newState });
-        window.location.reload();
-      }
-      setLocalStorageItem(SHOW_MENU, "true");
+    try {
+      if (!integration) throw new Error(t("donationError"));
+      await donate(
+        integration?.id,
+        nonProfit.id,
+        email,
+        PLATFORM,
+        getExternalIdFromLocationSearch(),
+        utmParams.utmSource,
+        utmParams.utmMedium,
+        utmParams.utmCampaign,
+      );
+      destroyVoucher();
+      if (onSuccess) onSuccess();
+    } catch (e: any) {
+      logError(e);
+      if (onError) onError(e);
+      const failedKey =
+        e.response?.status === 403 ? "blockedDonation" : "failedDonation";
+      const newState = {
+        [failedKey]: true,
+        message: e.response?.data?.formatted_message || e.message,
+      };
+      navigateTo({ pathname: "/causes", state: newState });
+      window.location.reload();
     }
+    setLocalStorageItem(SHOW_MENU, "true");
   }
 
   return { handleDonate };

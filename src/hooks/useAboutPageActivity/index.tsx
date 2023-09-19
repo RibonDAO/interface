8fa52c0f90440d0f7a38d9163946e8ec106673b5
Henrique Barbosa <henrikleod@gmail.com>
2023-09-19 11:37:23 -0300

    feat: about page view task

--- src/hooks/useAboutPageActivity/index.tsx
+++ src/hooks/useAboutPageActivity/index.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
+import { useTasksContext } from "contexts/tasksContext";
+import { TASKS } from "utils/constants/Tasks";
 
 export const HAS_SEEN_ABOUT_PAGE_TODAY_KEY = "HAS_SEEN_ABOUT_PAGE_TODAY_KEY";
 
 function useAboutPageActivity() {
   const [hasSeenAboutPageToday, setHasSeenAboutPageToday] = useState(false);
   const location = useLocation();
+  const { registerAction, tasksState } = useTasksContext();
 
   useEffect(() => {
     const hasSeenToday = localStorage.getItem(HAS_SEEN_ABOUT_PAGE_TODAY_KEY);
@@ -20,10 +23,25 @@ function useAboutPageActivity() {
     }
   }, [location]);
 
+  const setTaskDone = () => {
+    const pageViewTask = TASKS.filter(
+      (task) => task.title === "about_page_view",
+    )[0];
+
+    const done = tasksState?.find(
+      (task) => task?.id === pageViewTask?.id,
+    )?.done;
+
+    if (!done) {
+      registerAction("about_page_view");
+    }
+  };
+
   const setHasSeenToday = () => {
     const today = new Date().toLocaleDateString();
     localStorage.setItem(HAS_SEEN_ABOUT_PAGE_TODAY_KEY, today);
     setHasSeenAboutPageToday(true);
+    setTaskDone();
   };
 
   return {

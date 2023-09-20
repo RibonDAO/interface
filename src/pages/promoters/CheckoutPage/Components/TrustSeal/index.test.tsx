adc388f0d07bba534e7119d51143ca11ca4a3a4d
Nathiely Macedo <nathielymm@gmail.com>
2023-09-20 20:16:29 -0300

    fix: test

--- src/pages/promoters/CheckoutPage/Components/TrustSeal/index.test.tsx
+++ src/pages/promoters/CheckoutPage/Components/TrustSeal/index.test.tsx
@@ -2,6 +2,12 @@ import { renderComponent } from "config/testUtils";
 import { expectTextToBeInTheDocument } from "config/testUtils/expects";
 import TrustSeal from ".";
 
+jest.mock("@growthbook/growthbook-react", () => ({
+  useExperiment: () => ({
+    variation: "trustSeal",
+  }),
+}));
+
 describe("TrustSeal", () => {
   it("should render without error", () => {
     renderComponent(<TrustSeal />);

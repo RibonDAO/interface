4b5b95eb0e6087f2c0df7bf51e2aeb47ff1f7e65
letbueno <leticia.bueno.martins@gmail.com>
2023-09-26 08:47:07 -0300

    fix: remove event

--- src/pages/users/ForYouPage/ForYouMenu/NewsSection/RibonArticleComponent/index.tsx
+++ src/pages/users/ForYouPage/ForYouMenu/NewsSection/RibonArticleComponent/index.tsx
@@ -1,6 +1,4 @@
 import { useTranslation } from "react-i18next";
-import { useEffect } from "react";
-import { logEvent } from "lib/events";
 
 import ArticleLayout from "components/moleculars/layouts/ArticleLayout";
 import { RibonOnboarding } from "utils/constants/Articles";
@@ -9,10 +7,6 @@ function RibonArticleOnboarding() {
   const { t } = useTranslation("translation", {
     keyPrefix: "forYouPage.newsSection",
   });
-
-  useEffect(() => {
-    logEvent("P15_view");
-  }, []);
 
   return <ArticleLayout article={RibonOnboarding(t)} readMoreText="" />;
 }

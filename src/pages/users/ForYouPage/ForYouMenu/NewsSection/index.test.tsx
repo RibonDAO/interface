fb1f3b4409d8ebb76bad8116077de14d95d4e531
Thais correa <110051338+thaisribon@users.noreply.github.com>
2023-09-26 10:27:57 -0300

    Revert "Revert "feat: adds block news""

--- src/pages/users/ForYouPage/ForYouMenu/NewsSection/index.test.tsx
+++ src/pages/users/ForYouPage/ForYouMenu/NewsSection/index.test.tsx
@@ -1,13 +1,42 @@
-import { renderComponent } from "config/testUtils";
-import { expectTextToBeInTheDocument } from "config/testUtils/expects";
+import { useCanDonate } from "@ribon.io/shared";
+import { renderComponent, waitForPromises } from "config/testUtils";
+import {
+  expectTextNotToBeInTheDocument,
+  expectTextToBeInTheDocument,
+} from "config/testUtils/expects";
+import ArticleFactory from "config/testUtils/factories/articleFactory";
 import NewsSection from ".";
 
-describe("NewsSection", () => {
-  it("should render without error", () => {
-    renderComponent(<NewsSection />);
+const mockArticle = ArticleFactory({ title: "Environment", id: 1 });
 
-    expectTextToBeInTheDocument(
-      "Download our app and check our selection of heart warming news",
-    );
+jest.mock("@ribon.io/shared/hooks", () => ({
+  __esModule: true,
+  ...jest.requireActual("@ribon.io/shared/hooks"),
+  useArticles: () => ({
+    getUserArticles: () => [mockArticle],
+  }),
+  useCanDonate: jest.fn(),
+}));
+
+describe("NewsSection", () => {
+  describe("when user has donated", () => {
+    it("renders news", async () => {
+      (useCanDonate as jest.Mock).mockReturnValue({ canDonate: false });
+      renderComponent(<NewsSection />);
+      await waitForPromises();
+
+      expectTextNotToBeInTheDocument("Donate to read good news");
+    });
+  });
+
+  describe("when user has not donated", () => {
+    it("renders blocked section", async () => {
+      (useCanDonate as jest.Mock).mockReturnValue({ canDonate: true });
+
+      renderComponent(<NewsSection />);
+      await waitForPromises();
+
+      expectTextToBeInTheDocument("Donate to read good news");
+    });
   });
 });

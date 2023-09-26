09ab04305360972bbfe02d52fa133a2a51d0c2a3
Thais correa <110051338+thaisribon@users.noreply.github.com>
2023-09-26 10:13:26 -0300

    Revert "feat: adds block news"

--- src/pages/users/ForYouPage/ForYouMenu/NewsSection/index.tsx
+++ src/pages/users/ForYouPage/ForYouMenu/NewsSection/index.tsx
@@ -1,13 +1,8 @@
+import theme from "styles/theme";
 import { useTranslation } from "react-i18next";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { logEvent } from "lib/events";
-import ArticleLayout from "components/moleculars/layouts/ArticleLayout";
-import { useArticles, useCanDonate } from "@ribon.io/shared/hooks";
-import { Article } from "@ribon.io/shared/types";
-import useNavigation from "hooks/useNavigation";
-import { useIntegrationId } from "hooks/useIntegrationId";
-import extractUrlValue from "lib/extractUrlValue";
-import { PLATFORM } from "utils/constants";
+import AppDownloadTemplate from "pages/users/AppDownloadPage/AppDownloadTemplate";
 import NewsImage from "./assets/news-image.svg";
 import * as S from "./styles";
 
@@ -16,60 +11,26 @@ function NewsSection() {
     keyPrefix: "forYouPage.newsSection",
   });
 
-  const [articles, setArticles] = useState<Article[]>([]);
-
-  const { getUserArticles } = useArticles();
-  const integrationId = useIntegrationId();
-  const { history, navigateTo } = useNavigation();
-  const externalId = extractUrlValue("external_id", history.location.search);
-  const { canDonate } = useCanDonate(integrationId, PLATFORM, externalId);
-
-  useEffect(() => {
-    const fetchArticles = async () => {
-      const currentArticles = await getUserArticles();
-      setArticles(currentArticles);
-    };
-
-    fetchArticles();
-  }, []);
-
   useEffect(() => {
     logEvent("P15_view");
   }, []);
 
   function renderPage() {
-    if (!canDonate) {
-      return (
-        <S.Container>
-          <S.ArticlesContainer>
-            {articles &&
-              articles.map((article) => (
-                <ArticleLayout
-                  key={article.id}
-                  article={article}
-                  readMoreText={t("openPostButtonText")}
-                />
-              ))}
-          </S.ArticlesContainer>
-        </S.Container>
-      );
-    }
-
     return (
-      <S.BlockedContainer>
-        <S.Image src={NewsImage} />
-        <S.TextContainer>
-          <S.Title>{t("title")}</S.Title>
-          <S.Text>{t("text")}</S.Text>
-        </S.TextContainer>
-        <S.Button
-          onClick={() => {
-            navigateTo("/causes");
+      <S.Container>
+        <AppDownloadTemplate
+          title={t("title")}
+          image={NewsImage}
+          hasBackButton={false}
+          description={t("description")}
+          firstButton={{
+            text: t("buttonDownloadApp"),
+            backgroundColor: theme.colors.brand.primary[800],
+            textColor: theme.colors.neutral10,
           }}
-        >
-          {t("donateButtonText")}
-        </S.Button>
-      </S.BlockedContainer>
+          spacingTopDonationFlow
+        />
+      </S.Container>
     );
   }
 

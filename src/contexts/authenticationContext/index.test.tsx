93bd41d2ee984ebf794c1ef41d3891e62edfc6eb
Nathiely Macedo <nathielymm@gmail.com>
2023-10-19 15:27:43 -0300

    feat: adds authentication context

---
+++ src/contexts/authenticationContext/index.test.tsx
@@ -0,0 +1,15 @@
+import { renderComponent } from "config/testUtils";
+import { expectTextToBeInTheDocument } from "config/testUtils/expects";
+import { useAuthentication } from ".";
+
+function AuthenticationTestPage(){
+  useAuthentication();
+  return <div>Authentication</div>
+}
+
+describe("useAuthentication", () => {
+  it("renders without error", () => {
+    renderComponent(<AuthenticationTestPage />);
+    expectTextToBeInTheDocument("Authentication");
+  });
+});
---
+++ src/contexts/authenticationContext/index.tsx
@@ -0,0 +1,61 @@
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import { REFRESH_TOKEN_KEY, TOKEN_KEY } from "utils/constants";
+import { getCookiesItem, removeCookiesItem } from "lib/cookies";
+
+// todo: create goggle, apple and magic link login
+export interface IAuthenticationContext {
+  accessToken: string | null;
+  logout: () => void;
+}
+
+export type Props = {
+  children: JSX.Element[] | JSX.Element;
+};
+
+export const AuthenticationContext = createContext<IAuthenticationContext>(
+  {} as IAuthenticationContext,
+);
+
+function AuthenticationProvider({ children }: Props) {
+  const [accessToken] = useState(getCookiesItem(TOKEN_KEY));
+
+  function logout() {
+    removeCookiesItem(TOKEN_KEY);
+    removeCookiesItem(REFRESH_TOKEN_KEY);
+    // todo: navigate to public page
+  }
+
+  useEffect(() => {
+    if (!accessToken) {
+      logout();
+    }
+  }, [accessToken]);
+
+  const authenticationObject: IAuthenticationContext = useMemo(
+    () => ({
+      logout,
+      accessToken,
+    }),
+    [accessToken],
+  );
+
+  return (
+    <AuthenticationContext.Provider value={authenticationObject}>
+      {children}
+    </AuthenticationContext.Provider>
+  );
+}
+
+export default AuthenticationProvider;
+
+export const useAuthentication = () => {
+  const context = useContext(AuthenticationContext);
+
+  if (!context) {
+    throw new Error(
+      "useAuthentication must be used within AuthenticationProvider",
+    );
+  }
+
+  return context;
+};

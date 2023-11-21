ac066d06ab359f71e906834cfb468072630e5611
Thais Correa <thais@ribon.io>
2023-11-21 15:11:23 -0300

    feat: logout

--- src/layouts/LayoutHeader/LogoutItem/index.test.tsx
+++ src/layouts/LayoutHeader/LogoutItem/index.test.tsx
@@ -1,13 +1,12 @@
-import { screen } from "@testing-library/react";
-import { clickOn, renderComponent, waitForPromises } from "config/testUtils";
+import { clickOn, renderComponent } from "config/testUtils";
 import {
   expectTextToBeInTheDocument,
   expectPageToNavigateTo,
-  expectTextNotToBeInTheDocument,
 } from "config/testUtils/expects";
 import LogoutItem from ".";
 
 const closeMenu = jest.fn();
+const reload = jest.fn();
 
 describe("LogoutItem", () => {
   it("should render without errors", () => {
@@ -17,6 +16,10 @@ describe("LogoutItem", () => {
 
   describe("when the sign out button is clicked", () => {
     beforeEach(() => {
+      Object.defineProperty(window, "location", {
+        configurable: true,
+        value: { reload },
+      });
       renderComponent(<LogoutItem closeMenu={closeMenu} />, {
         currentUserProviderValue: {
           currentUser: { email: "user@email.com", id: 1 },
@@ -36,11 +39,9 @@ describe("LogoutItem", () => {
       expectTextToBeInTheDocument("user@email.com");
     });
 
-    it("Confirms sign out", async () => {
-      const signoutButton = screen.getByTestId("DialogPrimaryButton");
-      clickOn(signoutButton);
-      waitForPromises();
-      expectTextNotToBeInTheDocument("user@email.com");
+    it("Confirms sign out", () => {
+      clickOn("Sign out");
+      expect(reload).toHaveBeenCalled();
     });
 
     it("Signs out and returns to initial page", () => {

0e5c8dbfe68a8fee0fbc9c8aeb714a33efc9e3c9
letbueno <leticia.bueno.martins@gmail.com>
2023-11-21 10:55:24 -0300

    fix: remove modal email sent

--- src/layouts/LayoutHeader/DeleteAccountItem/index.tsx
+++ src/layouts/LayoutHeader/DeleteAccountItem/index.tsx
@@ -3,9 +3,8 @@ import { useTranslation } from "react-i18next";
 import { useCurrentUser } from "contexts/currentUserContext";
 import ArrowRight from "assets/icons/arrow-right-blue-icon.svg";
 import CardIconText from "components/moleculars/cards/CardIconText";
-import { useUsers, useUserV1Config } from "@ribon.io/shared/hooks";
+import { useUserV1Config } from "@ribon.io/shared/hooks";
 import ModalDialog from "components/moleculars/modals/ModalDialog";
-import { theme } from "@ribon.io/shared/styles";
 import { logEvent } from "lib/events";
 import { useAuthentication } from "contexts/authenticationContext";
 import * as S from "./styles";
@@ -15,15 +14,13 @@ function DeleteAccountItem(): JSX.Element {
     keyPrefix: "layouts.layoutHeader.deleteAccountItem",
   });
 
-  const { logoutCurrentUser, signedIn } = useCurrentUser();
-  const [emailSentModalVisible, setEmailSentModalVisible] = useState(false);
+  const { logoutCurrentUser } = useCurrentUser();
   const [deleteAccountModalVisible, setDeleteAccountModalVisible] =
     useState(false);
+
   const { accessToken } = useAuthentication();
 
-  const { sendDeleteAccountEmail } = useUserV1Config();
-  const { sendDeleteAccountEmail: sendDeleteAccountEmailNotAuthenticate } =
-    useUsers();
+  const { deleteUser } = useUserV1Config();
 
   function handleConfirmation() {
     setDeleteAccountModalVisible(true);
@@ -31,15 +28,8 @@ function DeleteAccountItem(): JSX.Element {
 
   function handleDeleteAccount() {
     logEvent("deleteAccountConfirmBtn_click");
-    setEmailSentModalVisible(true);
     setDeleteAccountModalVisible(false);
-
-    if (accessToken && signedIn) {
-      sendDeleteAccountEmail();
-    } else {
-      sendDeleteAccountEmailNotAuthenticate();
-    }
-
+    deleteUser(accessToken);
     logoutCurrentUser();
   }
 
@@ -66,18 +56,6 @@ function DeleteAccountItem(): JSX.Element {
         secondaryButton={{
           text: t("cancelModalButton"),
           onClick: () => setDeleteAccountModalVisible(false),
-        }}
-      />
-      <ModalDialog
-        visible={emailSentModalVisible}
-        title={t("emailSentTitle")}
-        description={t("emailSentDescription")}
-        type="success"
-        icon="check"
-        iconColor={theme.colors.brand.primary[600]}
-        secondaryButton={{
-          text: t("cancelModalButton"),
-          onClick: () => setEmailSentModalVisible(false),
         }}
       />
     </S.Container>

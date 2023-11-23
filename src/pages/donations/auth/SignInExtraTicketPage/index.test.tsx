56607ebe6986834e42a16292aa8fe8f6c4dd289d
mendesmcmg <claraplanejamento@gmail.com>
2023-11-23 12:47:52 -0300

    test: mocks google login

--- src/pages/donations/auth/SignInExtraTicketPage/index.test.tsx
+++ src/pages/donations/auth/SignInExtraTicketPage/index.test.tsx
@@ -2,6 +2,10 @@ import { renderComponent } from "config/testUtils";
 import { expectTextToBeInTheDocument } from "config/testUtils/expects";
 import SignInExtraTicketPage from ".";
 
+jest.mock("@react-oauth/google", () => ({
+  useGoogleLogin: () => {},
+}));
+
 describe("SignInExtraTicketPage", () => {
   it("should render without error", () => {
     renderComponent(<SignInExtraTicketPage />);

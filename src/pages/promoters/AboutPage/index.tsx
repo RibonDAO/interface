b089aa43ec2b2d4da49262c8edeff5a90de76c1b
Henrique Barbosa <henrikleod@gmail.com>
2023-11-27 08:34:36 -0300

    fix: link param

--- src/pages/promoters/AboutPage/index.tsx
+++ src/pages/promoters/AboutPage/index.tsx
@@ -10,7 +10,7 @@ function AboutPage(): JSX.Element {
   }, []);
 
   const pageSlug =
-    "https://projetos.ribon.io/dia-de-doar?integration_id=6227d432-32ac-4a98-a14f-2f4a05c3ba4a&utm_source=ribon&utm_medium=navbar&utm_campaign=dia_de_doar";
+    "dia-de-doar?integration_id=6227d432-32ac-4a98-a14f-2f4a05c3ba4a&utm_source=ribon&utm_medium=navbar&utm_campaign=dia_de_doar";
 
   return (
     <S.Container

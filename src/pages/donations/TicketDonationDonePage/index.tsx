84c9053c7034730ab1d7bd64d16376b2399c2d3d
Nathiely Macedo <nathielymm@gmail.com>
2023-11-28 13:34:21 -0300

    fix: adds validation

--- src/pages/donations/TicketDonationDonePage/index.tsx
+++ src/pages/donations/TicketDonationDonePage/index.tsx
@@ -65,17 +65,20 @@ function TicketDonationDonePage(): JSX.Element {
 
   const { refetch } = useFirstAccessToIntegration(integrationId);
 
-  const shouldShowEmailCheckbox = useCallback(
-    () =>
-      (Number(userStatistics?.totalTickets) <=
-        quantityOfDonationsToShowEmailCheckbox ||
-        Number(userStatistics?.totalTickets) %
-          quantityOfDonationsToShowEmailCheckbox ===
-          0 ||
-        Number(userStatistics?.totalTickets) === firstDonation) &&
-      !config?.allowedEmailMarketing,
-    [userStatistics, config],
-  );
+  const shouldShowEmailCheckbox = useCallback(() => {
+    if (userStatistics && config) {
+      return (
+        (Number(userStatistics.totalTickets) <=
+          quantityOfDonationsToShowEmailCheckbox ||
+          Number(userStatistics.totalTickets) %
+            quantityOfDonationsToShowEmailCheckbox ===
+            0 ||
+          Number(userStatistics.totalTickets) === firstDonation) &&
+        !config.allowedEmailMarketing
+      );
+    }
+    return false;
+  }, [userStatistics, config]);
 
   useEffect(() => {
     refetchStatistics();
@@ -130,6 +133,8 @@ function TicketDonationDonePage(): JSX.Element {
     </>
   );
 
+  const image =
+    "https://ribon-core-api-production.s3.amazonaws.com/wwtniqmqa54koan6s3p1we7izl8y?response-content-disposition=inline%3B%20filename%3D%22Captura%20de%20Tela%202022-09-05%20a%253Fs%2010.37.14.jpg%22%3B%20filename%2A%3DUTF-8%27%27Captura%2520de%2520Tela%25202022-09-05%2520a%25CC%2580s%252010.37.14.jpg&response-content-type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA42ZX7UC72NBLHNXM%2F20231128%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231128T141411Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=4ee61b8d6ae17c7fb4d3da0903b9e68da9fc70b447ab8fda0edd4aacc80027ac";
   const renderImpactValue = () => oldImpactFormat();
 
   return (
@@ -138,7 +143,7 @@ function TicketDonationDonePage(): JSX.Element {
       <S.ImageContainer>
         <IconsAroundImage
           isInfiniteAnimation={false}
-          imageSrc={nonProfit?.mainImage}
+          imageSrc={image}
           iconAnimationYellow={ConfirmationNumberYellow}
           iconAnimationPink={ConfirmationNumberPink}
           iconAnimationGreen={ConfirmationNumberGreen}

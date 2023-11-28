c0b754987c781df1ff2883741edb6528c706fdae
letbueno <leticia.bueno.martins@gmail.com>
2023-11-28 08:15:28 -0300

    fix: non profits list

--- src/pages/donations/CausesPage/NonProfitsList/index.tsx
+++ src/pages/donations/CausesPage/NonProfitsList/index.tsx
@@ -1,15 +1,12 @@
 import CardCenterImageButton from "components/moleculars/cards/CardCenterImageButton";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import useNavigation from "hooks/useNavigation";
-import { RIBON_COMPANY_ID } from "utils/constants";
 import { logEvent } from "lib/events";
 import { Currencies, NonProfit } from "@ribon.io/shared/types";
 import SliderCardsEnhanced from "components/moleculars/sliders/SliderCardsEnhanced";
 import useVoucher from "hooks/useVoucher";
 import useFormattedImpactText from "hooks/useFormattedImpactText";
-import { isFirstAccess } from "lib/onboardingFirstAccess";
-import { useCurrentUser } from "contexts/currentUserContext";
 import causeIllustration from "assets/images/direct-illustration.svg";
 import { useIntegrationId } from "hooks/useIntegrationId";
 import { useOffers } from "@ribon.io/shared/hooks";
@@ -34,7 +31,6 @@ function NonProfitsList({ nonProfits, canDonate }: Props): JSX.Element {
 
   const { formattedImpactText } = useFormattedImpactText();
   const { isVoucherAvailable } = useVoucher();
-  const { signedIn } = useCurrentUser();
 
   const canDonateAndHasVoucher = canDonate && isVoucherAvailable();
 
@@ -87,7 +83,7 @@ function NonProfitsList({ nonProfits, canDonate }: Props): JSX.Element {
       target_id: nonProfit.id.toString(),
       currency: currentLang === "pt-BR" ? "BRL" : "USD",
       subscription: "false",
-      from: "DirectCardNgo",
+      from: "CausesPage",
     });
 
     navigateTo({
@@ -96,27 +92,13 @@ function NonProfitsList({ nonProfits, canDonate }: Props): JSX.Element {
     });
   };
 
-  const isCheckoutButtonVisible = useCallback(() => {
-    const isRibonIntegration = integrationId?.toString() === RIBON_COMPANY_ID;
-    const isNotFirstAccess = !isFirstAccess(signedIn);
-
-    return Boolean(isRibonIntegration && isNotFirstAccess);
-  }, [integrationId]);
-
   function handleButtonClick(nonProfit: NonProfit, from: string) {
     if (canDonateAndHasVoucher) {
       logEvent("donateTicketBtn_start", {
         nonProfitId: nonProfit.id,
         from,
       });
-      if (signedIn) {
-        navigateTo({ pathname: "/signed-in", state: { nonProfit } });
-      } else {
-        navigateTo({
-          pathname: "/donation/auth/sign-in",
-          state: { nonProfit },
-        });
-      }
+      navigateTo({ pathname: "confirm-donation", state: { nonProfit } });
     } else {
       navigateToCheckout(nonProfit);
     }
@@ -153,23 +135,19 @@ function NonProfitsList({ nonProfits, canDonate }: Props): JSX.Element {
                     buttonText={
                       canDonateAndHasVoucher
                         ? t("donateText")
-                        : t("donateBlockedText")
+                        : t("doMore", {
+                            value: currentOffer()?.price ?? "1000",
+                          })
                     }
                     onClickButton={() =>
                       handleButtonClick(nonProfit, "nonProfitCard")
                     }
                     onClickImage={() => handleImageClick(nonProfit)}
-                    softDisabled={!canDonateAndHasVoucher}
                     infoTextTop={nonProfit.name}
                     infoTextBottom={nonProfit.cause?.name}
                     infoText={
                       nonProfit.stories?.length ? t("learnMore") : undefined
                     }
-                    secondButtonProps={{
-                      text: t("doMore", { value: currentOffer()?.price }),
-                      onClick: () => navigateToCheckout(nonProfit),
-                      visible: isCheckoutButtonVisible(),
-                    }}
                     fullWidth
                   />
                 </S.CardWrapper>

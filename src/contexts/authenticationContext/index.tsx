1b36046864909274d1eda4ad46ac7fa7abd0a1e6
Thais Correa <thais@ribon.io>
2023-11-30 17:02:04 -0300

    fix: send auth email integration id

--- src/contexts/authenticationContext/index.tsx
+++ src/contexts/authenticationContext/index.tsx
@@ -1,5 +1,9 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
-import { REFRESH_TOKEN_KEY, ACCESS_TOKEN_KEY } from "utils/constants";
+import {
+  REFRESH_TOKEN_KEY,
+  ACCESS_TOKEN_KEY,
+  INTEGRATION_AUTH_ID,
+} from "utils/constants";
 import {
   getCookiesItem,
   removeCookiesItem,
@@ -143,6 +147,7 @@ function AuthenticationProvider({ children }: Props) {
       const response = await userAuthenticationApi.postSendAuthenticationEmail(
         email,
         accountId,
+        INTEGRATION_AUTH_ID,
       );
       if (onSuccess) onSuccess();
 
--- src/pages/donations/auth/SignInByTokenPage/index.tsx
+++ src/pages/donations/auth/SignInByTokenPage/index.tsx
@@ -3,23 +3,34 @@ import extractUrlValue from "lib/extractUrlValue";
 import { useLocation } from "react-router";
 import { useAuthentication } from "contexts/authenticationContext";
 import Loader from "components/atomics/Loader";
-import ReceiveExtraTicketPage from "pages/donations/auth/ReceiveExtraTicketPage";
 import useNavigation from "hooks/useNavigation";
+import { useFirstAccessToIntegration } from "@ribon.io/shared/hooks";
+import { INTEGRATION_AUTH_ID } from "utils/constants";
 
 function SignInByMagicLink(): JSX.Element {
   const { search } = useLocation();
   const { navigateTo } = useNavigation();
-  const { signInByMagicLink, loading } = useAuthentication();
+  const { signInByMagicLink } = useAuthentication();
 
   useEffect(() => {
     async function authenticate() {
       const authToken = extractUrlValue("authToken", search) ?? "";
       const id = extractUrlValue("id", search) ?? "";
+      const extraTicket = extractUrlValue("extra_ticket", search) ?? "";
+      const isFistAccessToAuthIntegration =
+        useFirstAccessToIntegration(INTEGRATION_AUTH_ID);
 
       if (id && authToken) {
         signInByMagicLink({
           authToken,
           id,
+          onSuccess: () => {
+            if (extraTicket === "true" && isFistAccessToAuthIntegration) {
+              navigateTo("/receive-extra-ticket");
+            } else {
+              navigateTo("/causes");
+            }
+          },
           onError: () => {
             navigateTo({
               pathname: "/expired-link",
@@ -34,7 +45,7 @@ function SignInByMagicLink(): JSX.Element {
     authenticate();
   }, []);
 
-  return loading ? <Loader /> : <ReceiveExtraTicketPage />;
+  return <Loader />;
 }
 
 export default SignInByMagicLink;

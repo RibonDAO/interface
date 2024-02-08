57f4ec59b74657fc28c5646a56c01b6099110f86
Henrique Barbosa <henrikleod@gmail.com>
2024-02-08 19:07:45 -0300

    feat: dynamic qrcode

--- src/pages/users/AppDownloadPage/AppDownloadTemplate/index.tsx
+++ src/pages/users/AppDownloadPage/AppDownloadTemplate/index.tsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import useBreakpoint from "hooks/useBreakpoint";
-import { APP_LINK, IOS_APP_LINK, ANDROID_APP_LINK } from "utils/constants";
+import {
+  APP_LINK,
+  IOS_APP_LINK,
+  ANDROID_APP_LINK,
+  DAPP_URL,
+} from "utils/constants";
 import { useTranslation } from "react-i18next";
 import { formatCountdown } from "lib/formatters/countdownFormatter";
 import { useCountdown } from "hooks/useCountdown";
+import { getUTMFromLocationSearch } from "lib/getUTMFromLocationSearch";
+import { QRCodeSVG } from "qrcode.react";
 import Icon from "components/atomics/Icon";
 import theme from "styles/theme";
 import { ButtonProps } from "components/atomics/buttons/Button";
 import { logEvent } from "lib/events";
 import AppleBadge from "./assets/apple-badge-sm.png";
 import GoogleBadge from "./assets/google-badge-sm.png";
-import QRCode from "./assets/qrcodeapp.svg";
-
 import * as S from "./styles";
 
 export type Props = {
@@ -83,6 +88,22 @@ function AppDownloadTemplate({
     window.open(ANDROID_APP_LINK);
   }
 
+  const buildLink = () => {
+    const utmParams = getUTMFromLocationSearch(window.location.search);
+    const queryParams = new URLSearchParams({
+      utm_source: utmParams.utmSource,
+      utm_medium: utmParams.utmMedium,
+      utm_campaign: utmParams.utmCampaign,
+    });
+
+    const redirectParams = new URLSearchParams({
+      redirect_url: APP_LINK,
+      event: "qrCodeButton_click",
+    });
+
+    return `${DAPP_URL}redirect?${queryParams.toString()}&${redirectParams.toString()}`;
+  };
+
   const render = () => {
     if (isMobile) {
       return (
@@ -107,7 +128,7 @@ function AppDownloadTemplate({
         <>
           <S.Badges>
             <S.ImageContainer>
-              <S.QRCode src={QRCode} />
+              <QRCodeSVG value={buildLink()} size={120} />
             </S.ImageContainer>
             <S.ImageContainer>
               <S.BorderContainer>

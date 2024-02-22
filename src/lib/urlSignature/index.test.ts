4096d67fdce0875e9b5c1cac51778d8ec2b51894
Henrique Barbosa <henrikleod@gmail.com>
2024-02-22 17:54:55 -0300

    fix: remove mock process env

--- src/lib/urlSignature/index.test.ts
+++ src/lib/urlSignature/index.test.ts
@@ -5,21 +5,7 @@ const VALID_SIG =
   "23e566e8a66b01df7753ed27d917fa63d8890ccb32c69301de407d7f963bb1ac";
 const INVALID_SIG = "invalid_sig";
 
-const originalEnv = process.env;
-
 describe("urlSignature", () => {
-  beforeEach(() => {
-    jest.resetModules();
-    process.env = {
-      ...originalEnv,
-      URL_SIGNATURE_KEY: "mock_key",
-    };
-  });
-
-  afterEach(() => {
-    process.env = originalEnv;
-  });
-
   describe("generateUrlSignature", () => {
     it("generates a valid signture", () => {
       const sig = generateUrlSignature(URL);

6962d58b6307203ab8bd98092d54f4ec2fd8d4d0
mendesmcmg <claraplanejamento@gmail.com>
2024-03-14 15:21:13 -0300

    feat: make post donation page go to app download

--- src/hooks/usePostTicketDonationNavigation/index.tsx
+++ src/hooks/usePostTicketDonationNavigation/index.tsx
@@ -1,72 +1,25 @@
-import { Cause, NonProfit } from "@ribon.io/shared/types";
-import { useCanDonate, useStatistics } from "@ribon.io/shared/hooks";
+import { NonProfit } from "@ribon.io/shared/types";
+import { useStatistics } from "@ribon.io/shared/hooks";
 import { useCurrentUser } from "contexts/currentUserContext";
-import { useIntegrationId } from "hooks/useIntegrationId";
 import useNavigation from "hooks/useNavigation";
-import extractUrlValue from "lib/extractUrlValue";
-import { useCallback, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { PLATFORM } from "utils/constants";
+import { useEffect } from "react";
 
 function usePostTicketDonationNavigation() {
-  const integrationId = useIntegrationId();
-  const { search } = useLocation();
-  const externalId = extractUrlValue("external_id", search);
-  const { donateApp } = useCanDonate(integrationId, PLATFORM, externalId);
   const { currentUser } = useCurrentUser();
   const { navigateTo } = useNavigation();
-  const {
-    userStatistics,
-    refetch: refetchStatistics,
-    isLoading,
-  } = useStatistics({
+  const { refetch: refetchStatistics } = useStatistics({
     userId: currentUser?.id,
   });
-
-  const quantityOfDonationsToShowDownload = 3;
-  const quantityOfDonationsToShowContribute = 5;
-  const firstDonation = 1;
-
-  const shouldShowAppDownload = useCallback(() => {
-    if (donateApp) return false;
-    return (
-      Number(userStatistics?.totalTickets) %
-        quantityOfDonationsToShowDownload ===
-        0 || Number(userStatistics?.totalTickets) === firstDonation,
-      Number(userStatistics?.totalTickets) %
-        quantityOfDonationsToShowDownload ===
-        0 || Number(userStatistics?.totalTickets) === firstDonation
-    );
-  }, [userStatistics, donateApp]);
-  const shouldShowContribute = useCallback(
-    () =>
-      Number(userStatistics?.totalTickets) %
-        quantityOfDonationsToShowContribute ===
-        0 || Number(userStatistics?.totalTickets) === firstDonation,
-    [userStatistics],
-  );
 
   useEffect(() => {
     refetchStatistics();
   }, [currentUser]);
 
-  function handleNavigate(nonProfit: NonProfit, cause?: Cause) {
-    if (shouldShowAppDownload()) {
-      return navigateTo({
-        pathname: "/app-download",
-        state: { nonProfit, showContribute: shouldShowContribute() },
-      });
-    } else if (!isLoading && shouldShowContribute()) {
-      return navigateTo({
-        pathname: "/post-donation",
-        state: { nonProfit, cause },
-      });
-    } else {
-      return navigateTo({
-        pathname: "/causes",
-        state: { cause },
-      });
-    }
+  function handleNavigate(nonProfit: NonProfit) {
+    return navigateTo({
+      pathname: "/app-download",
+      state: { nonProfit, showContribute: true },
+    });
   }
 
   return { handleNavigate };

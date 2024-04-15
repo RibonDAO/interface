da6cfcc1b06e8e1a78b22ab567f38f3976b4bde8
mendesmcmg <claraplanejamento@gmail.com>
2024-04-15 16:48:01 -0300

    feat: adds card border radius

--- src/components/moleculars/cards/Card/index.tsx
+++ src/components/moleculars/cards/Card/index.tsx
@@ -4,10 +4,11 @@ export type Props = {
   children: JSX.Element | JSX.Element[];
   border: boolean;
   backgroundColor: string;
+  borderRadius?: number;
 };
-function Card({ children, border, backgroundColor }: Props): JSX.Element {
+function Card({ children, border, backgroundColor, borderRadius }: Props): JSX.Element {
   return (
-    <S.Container backgroundColor={backgroundColor} border={border}>
+    <S.Container backgroundColor={backgroundColor} border={border} borderRadius={borderRadius}>
       {children}
     </S.Container>
   );

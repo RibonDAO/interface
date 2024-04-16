4ca331ffccc9e75a76f04d43aaba30d053e00e75
Jean Scalabrin <jean.scalabrin@koud.com.br>
2024-04-16 11:15:55 -0300

    chore: add events for buttons

--- src/pages/donations/GiveTicketV2Page/index.tsx
+++ src/pages/donations/GiveTicketV2Page/index.tsx
@@ -1,15 +1,17 @@
 import { useEffect } from "react";
 import { useIntegrationId } from "hooks/useIntegrationId";
 import { useIntegration } from "@ribon.io/shared/hooks";
+import useNavigation from "hooks/useNavigation";
 import { logEvent } from "lib/events";
 import RibonLogo from "assets/images/logo-ribon.svg";
-import LeftImage from "assets/images/bottom-left-shape-red.svg";
-import RightImage from "assets/images/top-right-shape.svg";
+import LeftImage from "assets/images/bottom-left-shape.svg";
+import RightImage from "assets/images/top-right-sun-shape.svg";
 import Button from "components/atomics/buttons/Button";
 import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router";
-import { RIBON_COMPANY_ID } from "utils/constants";
+import { APP_LINK, RIBON_COMPANY_ID } from "utils/constants";
 import extractUrlValue from "lib/extractUrlValue";
+import useBreakpoint from "hooks/useBreakpoint";
 import theme from "styles/theme";
 import Ticket from "./assets/ticket.svg";
 import * as S from "./styles";
@@ -18,10 +20,12 @@ function GiveTicketV2Page(): JSX.Element {
   const { t } = useTranslation("translation", {
     keyPrefix: "donations.giveTicketV2Page",
   });
+  const { navigateTo } = useNavigation();
   const integrationId = useIntegrationId();
   const { search } = useLocation();
   const externalId = extractUrlValue("external_id", search);
   const { integration } = useIntegration(integrationId);
+  const { isMobile } = useBreakpoint();
 
   useEffect(() => {
     logEvent("P35_view", { from: integration?.id });
@@ -29,10 +33,23 @@ function GiveTicketV2Page(): JSX.Element {
 
   const handleDownload = () => {
     logEvent("downloadCTA_click", { from: "firstScreen" });
+
+    if (isMobile) {
+      window.open(`${APP_LINK}?integration_id=${integrationId}`);
+      return;
+    }
+
+    navigateTo({
+      pathname: "/app-download",
+      state: {
+        cameFrom: "intro",
+      },
+    });
   };
 
   const handleStayBrowser = () => {
     logEvent("P35_getTicketBtn_click");
+    navigateTo("/onboarding/second-page");
   };
 
   const title = () => {
@@ -74,17 +91,25 @@ function GiveTicketV2Page(): JSX.Element {
           </S.Header>
 
           <S.ButtonContainer>
-            <S.FilledButton onClick={handleDownload}>
-              {t("downloadAppButton")}
-            </S.FilledButton>
-            <Button
-              text={t("stayInBrowserButton")}
-              textColor={theme.colors.brand.primary[600]}
-              backgroundColor="transparent"
-              borderColor={theme.colors.brand.primary[600]}
-              borderRadius="4px"
-              onClick={handleStayBrowser}
-            />
+            {isMobile ? (
+              <>
+                <S.FilledButton onClick={handleDownload}>
+                  {t("downloadAppButton")}
+                </S.FilledButton>
+                <Button
+                  text={t("stayInBrowserButton")}
+                  textColor={theme.colors.brand.primary[600]}
+                  backgroundColor="transparent"
+                  borderColor={theme.colors.brand.primary[600]}
+                  borderRadius="4px"
+                  onClick={handleStayBrowser}
+                />
+              </>
+            ) : (
+              <S.FilledButton onClick={handleStayBrowser}>
+                {t("nextButton")}
+              </S.FilledButton>
+            )}
           </S.ButtonContainer>
         </S.ContentContainer>
       </S.MainContainer>

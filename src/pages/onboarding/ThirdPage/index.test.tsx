9b6af0d1de8dff5ac51db0f52ee7c98bf5dab8ac
mendesmcmg <claraplanejamento@gmail.com>
2024-04-16 17:07:29 -0300

    fix: lint

--- src/pages/onboarding/ThirdPage/index.test.tsx
+++ src/pages/onboarding/ThirdPage/index.test.tsx
@@ -1,12 +1,17 @@
 import { clickOn, renderComponent } from "config/testUtils";
-import { expectPageToNavigateTo, expectTextToBeInTheDocument } from "config/testUtils/expects";
+import {
+  expectPageToNavigateTo,
+  expectTextToBeInTheDocument,
+} from "config/testUtils/expects";
 import ThirdPage from ".";
 
 describe("onboarding - ThirdPage", () => {
   it("should render without error", () => {
     renderComponent(<ThirdPage />);
-    
-    expectTextToBeInTheDocument("Before donating your ticket, here's a quick summary...");
+
+    expectTextToBeInTheDocument(
+      "Before donating your ticket, here's a quick summary...",
+    );
     expectTextToBeInTheDocument("What are tickets?");
     expectTextToBeInTheDocument("Who pays the tickets?");
     expectTextToBeInTheDocument("How to earn more tickets?");

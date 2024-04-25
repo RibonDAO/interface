836b949d71b2e0bff98e7e9266c2add360153101
Jean Scalabrin <jean.scalabrin@koud.com.br>
2024-04-25 09:13:20 -0300

    chore: change page extra ticket to send email only

--- src/pages/auth/SentMagicLinkEmailPage/index.tsx
+++ src/pages/auth/SentMagicLinkEmailPage/index.tsx
@@ -5,8 +5,6 @@ import RightImage from "assets/images/top-right-shape.svg";
 import { logEvent } from "lib/events";
 import { useLocation } from "react-router-dom";
 import useAvoidBackButton from "hooks/useAvoidBackButton";
-import { useFirstAccessToIntegration } from "@ribon.io/shared/hooks";
-import { INTEGRATION_AUTH_ID } from "utils/constants";
 import UserAvatar from "../assets/user-avatar.svg";
 import * as S from "./styles";
 
@@ -18,8 +16,6 @@ function SentMagicLinkEmailPage(): JSX.Element {
   const { t } = useTranslation("translation", {
     keyPrefix: "auth.sentMagicLinkEmailPage",
   });
-  const { isFirstAccessToIntegration } =
-    useFirstAccessToIntegration(INTEGRATION_AUTH_ID);
 
   useEffect(() => {
     logEvent("P29_view", {
@@ -41,14 +37,10 @@ function SentMagicLinkEmailPage(): JSX.Element {
           <S.MainImage src={UserAvatar} />
         </S.ImageContainer>
         <S.ContentContainer>
-          <S.Title>
-            {isFirstAccessToIntegration ? t("firstAccessTitle") : t("title")}
-          </S.Title>
-          <S.Text>
-            {isFirstAccessToIntegration
-              ? t("firstAccessText", { email })
-              : t("text", { email })}
-          </S.Text>
+          <S.Title>{t("title")}</S.Title>
+          <S.Text>{t("firstText")}</S.Text>
+          <S.EmailText>{email}</S.EmailText>
+          <S.Text>{t("secondText")}</S.Text>
         </S.ContentContainer>
       </div>
     </S.Container>

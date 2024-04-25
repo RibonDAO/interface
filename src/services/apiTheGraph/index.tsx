1a9f3cc4fb9d5099d368d532e5963b2e0a0447c0
letbueno <leticia.bueno.martins@gmail.com>
2024-04-25 09:41:08 -0300

    fix: remove unecessary code

--- src/services/apiTheGraph/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { THE_GRAPH_API } from "utils/constants";
-
-const APIURL = THE_GRAPH_API;
-
-export const client = new ApolloClient({
-  uri: APIURL,
-  cache: new InMemoryCache(),
-});

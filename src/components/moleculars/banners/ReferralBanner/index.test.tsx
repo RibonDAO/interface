947ab90cb304bc76f84c3711c805bb832757370c
Henrique Barbosa <henrikleod@gmail.com>
2024-05-06 17:25:16 -0300

    fix: broken test

--- src/components/moleculars/banners/ReferralBanner/index.test.tsx
+++ src/components/moleculars/banners/ReferralBanner/index.test.tsx
@@ -2,6 +2,22 @@ import { renderComponent } from "config/testUtils";
 import { expectTextToBeInTheDocument } from "config/testUtils/expects";
 import ReferralBanner from ".";
 
+jest.mock("@ribon.io/shared/hooks", () => ({
+  __esModule: true,
+  ...jest.requireActual("@ribon.io/shared/hooks"),
+  useUserProfile: () => ({
+    userProfile: () => ({
+      profile: {
+        photo: "photo",
+        name: "name",
+        user: {
+          email: "email",
+        },
+      },
+    }),
+  }),
+}));
+
 describe("Banner Component", () => {
   it("renders ReferralBanner with no errors", () => {
     renderComponent(<ReferralBanner />);

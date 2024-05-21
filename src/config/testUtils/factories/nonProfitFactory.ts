2d2ba5204d37c6e8a8a79ab50cfb281634f37fd2
mendesmcmg <claraplanejamento@gmail.com>
2024-05-21 17:51:38 -0300

    fix: removes double declaration

--- src/config/testUtils/factories/nonProfitFactory.ts
+++ src/config/testUtils/factories/nonProfitFactory.ts
@@ -27,8 +27,6 @@ function nonProfitFactory(params: Partial<NonProfit> = {}): NonProfit {
     logoDescription: "",
     impactTitle: "",
     coverImageDescription: "",
-    icon: "",
-    iconDescription: "",
   };
   return Object.assign(defaultValues, params) as NonProfit;
 }

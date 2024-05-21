0d564c3cb3ada412c22ef4a484e6ab936b64c07a
mendesmcmg <claraplanejamento@gmail.com>
2024-05-21 13:49:00 -0300

    refactor: moves FirstCard to nonProfitComponent

---
+++ src/pages/donations/CausesPage/NonProfitsListCarousel/NonProfitComponent/FirstCard/index.test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { clickOn, renderComponent } from "config/testUtils";
+import { NonProfit } from "@ribon.io/shared";
+import { expectTextToBeInTheDocument } from "config/testUtils/expects";
+import FirstCard from ".";
+
+const mockNonProfit = {
+  name: "Non Profit Test",
+  impactTitle: "Impact title test",
+  coverImage: "coverImage.png",
+} as NonProfit;
+
+const buttonOnClick = jest.fn();
+
+describe("FirstCard Component", () => {
+  beforeEach(() => {
+    renderComponent(
+      <FirstCard
+        nonProfit={mockNonProfit}
+        buttonOnClick={buttonOnClick}
+        buttonDisabled={false}
+        ticketsQuantity={2}
+      />,
+    );
+  });
+
+  it("should render texts without error", () => {
+    expectTextToBeInTheDocument("Non Profit Test");
+    expectTextToBeInTheDocument("Impact title test");
+    expectTextToBeInTheDocument("2");
+  });
+
+  it("should call a function when click on button", () => {
+    clickOn("Donate tickets");
+    expect(buttonOnClick).toHaveBeenCalled();
+  });
+});

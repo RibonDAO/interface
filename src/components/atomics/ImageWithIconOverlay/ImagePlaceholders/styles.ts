09b9823220e50b4ebd8d2322f30607a46d325dd4
mendesmcmg <claraplanejamento@gmail.com>
2024-06-19 17:15:34 -0300

    fix: css lint

--- src/components/atomics/ImageWithIconOverlay/ImagePlaceholders/styles.ts
+++ src/components/atomics/ImageWithIconOverlay/ImagePlaceholders/styles.ts
@@ -3,18 +3,17 @@ import styled from "styled-components";
 export const LeftImageContainer = styled.div`
   width: 104px;
   height: 104px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   position: absolute;
-  z-index: 199;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const RightContainer = styled.div`
   width: 128px;
   height: 128px;
   margin-left: -40px;
-  justify-content: center;
-  align-items: center;
   position: absolute;
+  align-items: center;
+  justify-content: center;
 `;
--- src/components/atomics/ImageWithIconOverlay/styles.ts
+++ src/components/atomics/ImageWithIconOverlay/styles.ts
@@ -8,38 +8,38 @@ export const Container = styled.div`
 export const LeftContainer = styled.div`
   width: 128px;
   height: 128px;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
 `;
 export const LeftImageContainer = styled.div`
   width: 104px;
   height: 104px;
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
 `;
 
 export const LeftImage = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: cover;
-  border-radius: 52px;
   border: 4px solid ${theme.colors.neutral10};
+  border-radius: 52px;
+  object-fit: cover;
 `;
 
 export const AvatarContainer = styled.div`
   width: 100%;
   height: 100%;
-  border-radius: 52px;
   border: 4px solid ${theme.colors.neutral10};
+  border-radius: 52px;
   background-color: ${theme.colors.neutral10};
 `;
 export const RightContainer = styled.div`
   width: 128px;
   height: 128px;
   margin-left: -40px;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
 `;
 export const RightImage = styled.img`
   width: 128px;

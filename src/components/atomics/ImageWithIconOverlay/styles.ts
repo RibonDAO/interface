0788d3bfdc6481dca9f2eb91012fcc5d7c8c28f6
mendesmcmg <claraplanejamento@gmail.com>
2024-06-19 18:43:57 -0300

    fix: fixes styling alignment

--- src/components/atomics/ImageWithIconOverlay/styles.ts
+++ src/components/atomics/ImageWithIconOverlay/styles.ts
@@ -5,27 +5,29 @@ export const Container = styled.div`
   display: flex;
   flex-direction: row;
 `;
+
 export const LeftContainer = styled.div`
   width: 128px;
   height: 128px;
   align-items: center;
   justify-content: center;
 `;
+
 export const LeftImageContainer = styled.div`
-  width: 104px;
-  height: 104px;
+  width: 118px;
+  height: 118px;
   display: flex;
   align-items: center;
   justify-content: center;
-`;
+  `;
 
 export const LeftImage = styled.img`
-  width: 100%;
-  height: 100%;
+  width: 118px;
+  height: 118px;
   border: 4px solid ${theme.colors.neutral10};
-  border-radius: 52px;
+  border-radius: 50%;
   object-fit: cover;
-`;
+  `;
 
 export const AvatarContainer = styled.div`
   width: 100%;
@@ -33,14 +35,16 @@ export const AvatarContainer = styled.div`
   border: 4px solid ${theme.colors.neutral10};
   border-radius: 52px;
   background-color: ${theme.colors.neutral10};
-`;
+  `;
+
 export const RightContainer = styled.div`
   width: 128px;
   height: 128px;
-  margin-left: -40px;
+  margin-left: -50px;
   align-items: center;
   justify-content: center;
 `;
+
 export const RightImage = styled.img`
   width: 128px;
   height: 128px;

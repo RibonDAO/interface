c8019dc4b6a0b06629e04cdc9f08c5dcc2d04766
letbueno <leticia.bueno.martins@gmail.com>
2024-06-20 13:28:09 -0300

    fix: all nonprofits

--- src/pages/donations/CausesPage/TagsSelectSection/index.tsx
+++ src/pages/donations/CausesPage/TagsSelectSection/index.tsx
@@ -17,6 +17,8 @@ function TagsSelectSection(): JSX.Element {
       setChosenTagIndex(index);
       if (tag.id !== 0) {
         setChosenTag(tag);
+      } else {
+        setChosenTag(undefined);
       }
     }
   };

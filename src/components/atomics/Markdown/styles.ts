f6bea4e99c9184775da213ec2a8534ebefebf2ac
mendesmcmg <claraplanejamento@gmail.com>
2024-06-24 16:12:22 -0300

    fix: removes theme spacing

--- src/components/atomics/Markdown/styles.ts
+++ src/components/atomics/Markdown/styles.ts
@@ -48,7 +48,7 @@ export const MarkdownContainer = styled.div`
 
   th,
   td {
-    padding: ${({ theme }) => theme.spacing(4)};
+    padding: 4px;
     font-weight: 400;
     text-align: start;
   }

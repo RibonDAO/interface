f7b477f49d02d6e172376a3d38d4fe9cc38c5f06
Nathiely Macedo <nathielymm@gmail.com>
2024-06-25 14:26:35 -0300

    fix: lint

--- src/pages/donations/SelectTicketsPage/styles.ts
+++ src/pages/donations/SelectTicketsPage/styles.ts
@@ -5,55 +5,57 @@ import ButtonComponent from "components/atomics/buttons/Button";
 
 export const Title = styled.p`
   ${stylizedDisplayXs}
+
   color: ${({ theme }) => theme.colors.brand.primary[900]};
 `;
 
 export const Container = styled.div`
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
 `;
 
 export const MainContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
   flex-grow: 1;
+  flex-direction: column;
   align-items: center;
+  justify-content: space-between;
 `;
 
 export const ImageContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   height: 100%;
+  display: flex;
   flex-grow: 1;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const ContentContainer = styled.div`
-  display: flex;
   width: 328px;
+  margin-bottom: 52px;
+  padding: ${({ theme }) => theme.spacing(16)};
+  display: flex;
   flex-direction: column;
+  gap: ${({ theme }) => theme.spacing(24)};
   align-items: center;
   text-align: center;
-  gap: ${({ theme }) => theme.spacing(24)};
-  padding: ${({ theme }) => theme.spacing(16)};
-  margin-bottom: 52px;
 `;
 
 export const Subtitle = styled.p`
   ${defaultBodyMdMedium}
+
   min-height: 48px;
   text-align: center;
   color: ${({ theme }) => theme.colors.neutral[500]};
 `;
 
 export const Button = styled(ButtonComponent)`
-  background-color: ${({ theme }) => theme.colors.brand.primary[600]};
   width: 100%;
   height: 48px;
-  border-radius: 12px;
   margin-top: ${({ theme }) => theme.spacing(8)};
+  border-radius: 12px;
+  background-color: ${({ theme }) => theme.colors.brand.primary[600]};
 `;
 
 export const TextContainer = styled.div`
@@ -63,7 +65,7 @@ export const TextContainer = styled.div`
 `;
 
 export const SliderContainer = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
-  width: 100%;
 `;

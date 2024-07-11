ff851a7d7dd5adb13674eef96c664945623bca47
Nathiely Macedo <nathielymm@gmail.com>
2024-07-11 11:41:36 -0300

    test: add test

--- src/pages/donations/CausesPage/MadeByRibonSection/index.test.tsx
+++ src/pages/donations/CausesPage/MadeByRibonSection/index.test.tsx
@@ -1,10 +1,26 @@
 import { renderComponent } from "config/testUtils/renders";
-import { expectTextToBeInTheDocument } from "config/testUtils/expects";
+import {
+  expectLogEventToHaveBeenCalledWith,
+  expectTextToBeInTheDocument,
+} from "config/testUtils/expects";
+import { clickOn } from "config/testUtils";
 import MadeByRibonSection from ".";
 
 describe("MadeByRibonPill", () => {
+  beforeEach(async () => {
+    await renderComponent(<MadeByRibonSection />);
+  });
+
   it("renders without error", () => {
-    renderComponent(<MadeByRibonSection />);
     expectTextToBeInTheDocument("Made by");
+  });
+
+  describe("on click", () => {
+    beforeEach(async () => {
+      clickOn("Made by");
+    });
+    it("should log the event", () => {
+      expectLogEventToHaveBeenCalledWith("madebyribon_click");
+    });
   });
 });
